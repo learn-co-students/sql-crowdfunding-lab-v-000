fa960859640a43400c13ba81094ce9b6aad2df69
frankNowinski <nowinski.frank@gmail.com>
2015-12-21 16:14:59 -0500

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -2,35 +2,35 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts
   "SELECT projects.title, SUM(pledges.amount)
   FROM projects
   JOIN pledges
-  ON projects.id = pledges.project_id GROUP BY projects.title;"
+  ON projects.id == pledges.project_id GROUP BY projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges
   "SELECT users.name, users.age, SUM(pledges.amount)
   FROM users
-  JOIN pledges 
+  JOIN pledges
   ON users.id = pledges.user_id GROUP BY users.name;"
 end
 
 def selects_the_titles_of_all_projects_that_have_met_their_funding_goal
-  "SELECT projects.title, (projects.funding_goal - SUM(pledges.amount)) AS remaining_amount
-  FROM projects 
-  JOIN pledges 
+  "SELECT projects.title, (projects.funding_goal - SUM(pledges.amount)) AS amount_left
+  FROM projects
+  JOIN pledges
   ON projects.id = pledges.project_id GROUP BY projects.title 
-  HAVING remaining_amount <= 0;"
+  HAVING amount_left <= 0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount
-  "SELECT users.name, SUM(pledges.amount) AS pledge_total
-  FROM users 
-  JOIN pledges 
-  ON users.id = pledges.user_id GROUP BY users.name ORDER BY pledge_total;"
+  "SELECT users.name, SUM(pledges.amount) AS total_pledges
+  FROM users
+  JOIN pledges
+  ON users.id = pledges.user_id GROUP BY users.name ORDER BY total_pledges;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   "SELECT projects.category, pledges.amount
-  FROM projects 
-  JOIN pledges 
+  FROM projects
+  JOIN pledges
   ON projects.id = pledges.project_id WHERE projects.category = 'music';"
 end
 
