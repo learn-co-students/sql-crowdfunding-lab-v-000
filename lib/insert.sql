4824fcfa2fc9d55d1c28da445f8d829228cc71d7
CalebKnox <knoxcaleb@aol.com>
2016-01-26 18:22:32 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,73 @@
+
+
+INSERT INTO users (name, age) VAlUES
+("bob", 2),
+("bill", 2),
+("jack", 3),
+("caleb",25),
+("becky", 58),
+("john", 44),
+("elmer", 92),
+("vivian", 102),
+("name", 2),
+("name", 3),
+("name", 4),
+("name", 5),
+("name", 6),
+("name", 7),
+("name", 8),
+("name", 9),
+("named", 10),
+("named", 11),
+("named", 12),
+("nameded", 13);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+("title", "category", "funding_goal", "start_Date", "end_date"),
+("title", "category", "funding_goal", "start_Date", "end_date"),
+("title", "category", "funding_goal", "start_Date", "end_date"),
+("title", "category", "funding_goal", "start_Date", "end_date"),
+("title", "category", "funding_goal", "start_Date", "end_date"),
+("title", "category", "funding_goal", "start_Date", "end_date"),
+("title", "category", "funding_goal", "start_Date", "end_date"),
+("title", "category", "funding_goal", "start_Date", "end_date"),
+("title", "category", "funding_goal", "start_Date", "end_date"),
+("title", "category", "funding_goal", "start_Date", "end_date");
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES
+(2, 2, 1),
+(2, 2, 1),
+(2, 2, 1),
+(2, 2, 1),
+(2, 2, 1),
+(2, 2, 1),
+(2, 2, 1),
+(2, 2, 1),
+(2, 2, 1),
+(2, 2, 1),
+(2, 2, 1),
+(2, 2, 1),
+(2, 2, 1),
+(2, 2, 1),
+(2, 2, 1),
+(2, 2, 1),
+(2, 2, 1),
+(2, 2, 1),
+(2, 2, 1),
+(2, 2, 1),
+(2, 2, 1),
+(2, 2, 1),
+(2, 2, 1),
+(2, 2, 1),
+(2, 2, 1),
+(2, 2, 1),
+(2, 2, 1),
+(2, 2, 1),
+(2, 2, 1),
+(2, 2, 1);
+
+
+
+
+
+
