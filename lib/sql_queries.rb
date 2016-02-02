f13f16ba459a01f1c9c8895252365742915d71b4
Kevin McCormack <wittyman37@yahoo.com>
2016-02-02 16:37:27 -0500

    4 tests left

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,11 +7,11 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts
-"Write your SQL query Here"
+"SELECT Projects.title, SUM(Pledges.amount) FROM projects INNER JOIN pledges ON Projects.id = Pledges.project_id GROUP BY Projects.title"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges
-"Write your SQL query Here"
+"SELECT Users.name, Users.age, SUM(Pledges.amount) FROM users INNER JOIN pledges ON Users.id = Pledges.user_id GROUP BY Users.name"
 end
 
 def selects_the_titles_of_all_projects_that_have_met_their_funding_goal
