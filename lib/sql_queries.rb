a87259b4a708a808a5b18c21984908c87807aefa
Kevin McCormack <wittyman37@yahoo.com>
2016-02-02 18:10:05 -0500

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,17 +15,17 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges
 end
 
 def selects_the_titles_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+"SELECT Projects.title, Projects.funding_goal-SUM(Pledges.amount) FROM projects INNER JOIN pledges ON Projects.id = Pledges.project_id GROUP BY Projects.title HAVING SUM(Pledges.amount) >= Projects.funding_goal"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount
-"Write your SQL query Here"
+"SELECT Users.name, SUM(Pledges.amount) FROM Pledges INNER JOIN Users ON Pledges.user_id = Users.id GROUP BY Users.name ORDER BY SUM(Pledges.amount)"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"SELECT Projects.category, Pledges.amount FROM Pledges INNER JOIN Projects ON Projects.id = Pledges.project_id WHERE category = 'music' "
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_book_category
-"Write your SQL query Here"
+"SELECT Projects.category, SUM(Pledges.amount) FROM Pledges INNER JOIN Projects ON Projects.id = Pledges.project_id WHERE category = 'books'"
 end
