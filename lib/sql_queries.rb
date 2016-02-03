d5d7527e550165784c09060cdabe3ec6a1a9d565
authorbeard <msnolan@gmail.com>
2016-02-03 11:59:58 -0500

    refactored/shortened code after checking solution.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -26,12 +26,12 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges
 end
 
 def selects_the_titles_of_all_projects_that_have_met_their_funding_goal
-  "SELECT projects.title, (projects.funding_goal - SUM(pledges.amount))
+  "SELECT projects.title, (projects.funding_goal - SUM(pledges.amount)) AS amount_left
    FROM projects
    JOIN pledges
    ON projects.id = pledges.project_id
    GROUP BY project_id
-   HAVING (projects.funding_goal - SUM(pledges.amount)) <= 0;"
+   HAVING amount_left <= 0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount
@@ -39,8 +39,8 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
    FROM pledges
    JOIN users
    ON pledges.user_id = users.id 
-   GROUP BY users.id
-   ORDER BY SUM (pledges.amount), users.name;"
+   GROUP BY users.name
+   ORDER BY SUM (pledges.amount);"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
