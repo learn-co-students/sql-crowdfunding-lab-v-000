5699c7665caecd77d077f31daba4d336f1749cc0
Heber Sandoval <he3ers@gmail.com>
2016-03-22 12:49:23 -0400

    Edit made on query

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,9 +23,9 @@ GROUP BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, projects.funding_goal - SUM(pledges.amount)
+"SELECT projects.title, SUM(pledges.amount) - projects.funding_goal
 FROM projects
-INNER JOIN pledges
+LEFT OUTER JOIN pledges
 ON projects.id = pledges.project_id
 GROUP BY projects.title HAVING SUM(pledges.amount) >= projects.funding_goal;"
 end
