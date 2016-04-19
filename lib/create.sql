8dded1e913f60e66cba92b79fb6d740954c5aad4
motti770 <mottigottlieb@gmail.com>
2016-04-19 21:30:22 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,18 @@
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal TEXT,
+  start_date TEXT,
+  end_date
+);
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE pledge (
+  id INTEGER PRIMARY KEY
+);
