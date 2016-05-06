fa8589c8bade7a334b2a5b1a8248c8b203859b80
theRingleman <sam.ringleman@gmail.com>
2016-05-05 21:37:53 -0600

    WIP

---
+++ lib/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+  VALUES ;
+INSERT INTO users (name, age)
+  VALUES ;
+INSERT INTO pledges (amount, user_id, project_id)
+  VALUES ;
