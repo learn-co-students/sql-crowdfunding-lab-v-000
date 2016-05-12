5b8834f1daccc7c44ec25e92a93b291be5f627a9
Michael Chen <michael.chen.1120@gmail.com>
2016-05-12 17:55:49 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO projects (title,category,funding_goal,start_date,end_date) VALUES
+("fast","cars",10000,"1/1/16","6/1/16"),("furious","racing",20000,"2/1/16","7/1/16")
+, ("need","speed",5000,"5/1/16","8/1/16"), ("big belly burger","food",10000,"1/1/16","12/1/16"),
+("kick","soccer club",70000,"4/15/16","6/1/16"), ("YO","startup",10000,"1/30/16","7/4/16"),
+("new Call of Duty","video games",200000,"4/20/16","8/30/16"), ("food 4 kids","charity",30000,"8/1/16","12/30/16"),
+("cheap watches","startup",15000,"7/1/16","11/1/16"), ("clicky keyboards","startup",3000,"3/1/16","6/1/16");
+
+INSERT INTO users (name,age) VALUES ("John",25),("Mike",27),("Will",21),("Chris",29),("Jenny",20),("Eddie",26),("Peter",29),("Lily",25),("Johnny",21)
+,("Hobbs",35),("Jason",31),("Mia",23),("Justin",24),("Roman",18),("Letty",20),("Calvin",30),("Richard",20),("Han",24),("Brian",18),("Dom",19);
+
+INSERT INTO pledges (amount,user_id,project_id) VALUES (100,1,1), (50,2,2),(200,3,3),(100,4,4),(100,5,5),(100,6,6),(100,7,7),(100,8,8),(100,9,9),(100,10,10)
+,(100,11,1),(100,12,2),(100,13,3),(100,14,4),(100,15,5),(100,16,6),(100,17,7),(100,18,8),(100,19,9),(100,20,10),(100,1,1),(100,2,2),(100,3,3),
+(100,4,4),(100,5,5),(100,6,6),(100,7,7),(100,8,8),(100,9,9),(100,10,10);
