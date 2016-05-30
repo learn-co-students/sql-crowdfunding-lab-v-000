74649e900739ceb37ffd43152aef7d2271197d29
hangloos <ericloos00@gmail.com>
2016-05-29 21:16:02 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,76 @@
+INSERT INTO projects VALUES (1,"project 1","construction",5000, "May 5, 2016","June 8, 2016" );
+INSERT INTO projects VALUES (2,"project 2","hiring",6000, "May 5, 2016","June 8, 2016" );
+INSERT INTO projects VALUES (3,"project 3","construction",5000, "May 5, 2016","June 8, 2016");
+INSERT INTO projects VALUES (4,"project 4","hiring",7000, "May 5, 2016","June 8, 2016" );
+INSERT INTO projects VALUES (5,"project 5","construction",5000, "May 5, 2016","June 8, 2016");
+INSERT INTO projects VALUES (6,"project 6","hiring",8000, "May 5, 2016","June 8, 2016");
+INSERT INTO projects VALUES (7,"project 7","financial",9000, "May 5, 2016","June 8, 2016");
+INSERT INTO projects VALUES (8,"project 8","hiring",4000, "May 5, 2016","June 8, 2016");
+INSERT INTO projects VALUES (9,"project 9","financial",5000, "May 5, 2016","June 8, 2016");
+INSERT INTO projects VALUES (10,"project 10","construction",3000, "May 5, 2016","June 8, 2016");
+
+INSERT INTO users VALUES(1,"eric",35);
+INSERT INTO users VALUES(2,"adam",34);
+INSERT INTO users VALUES(3,"steve",37);
+INSERT INTO users VALUES(4,"molly",39);
+INSERT INTO users VALUES(5,"lindsay",32);
+INSERT INTO users VALUES(6,"sarah",31);
+INSERT INTO users VALUES(7,"eric",32);
+INSERT INTO users VALUES(8,"erik",32);
+INSERT INTO users VALUES(9,"stephanie",32);
+INSERT INTO users VALUES(10,"jay",31);
+INSERT INTO users VALUES(11,"john",31);
+INSERT INTO users VALUES(12,"jennifer",31);
+INSERT INTO users VALUES(13,"sharon",31);
+INSERT INTO users VALUES(14,"trent",31);
+INSERT INTO users VALUES(15,"trace",36);
+INSERT INTO users VALUES(16,"susan",37);
+INSERT INTO users VALUES(17,"steve",38);
+INSERT INTO users VALUES(18,"allie",39);
+INSERT INTO users VALUES(19,"brett",35);
+INSERT INTO users VALUES(20,"eric",34);
+
+INSERT INTO pledges VALUES(1,1,2000,1);
+INSERT INTO pledges VALUES(2,3,1000,2);
+INSERT INTO pledges VALUES(3,5,4000,4);
+INSERT INTO pledges VALUES(4,6,4000,5);
+INSERT INTO pledges VALUES(5,7,3000,6);
+INSERT INTO pledges VALUES(6,8,2000,6);
+INSERT INTO pledges VALUES(7,9,1000,7);
+INSERT INTO pledges VALUES(8,2,5000,7);
+INSERT INTO pledges VALUES(9,2,1000,8);
+INSERT INTO pledges VALUES(10,1,500,2);
+INSERT INTO pledges VALUES(11,1,2000,1);
+INSERT INTO pledges VALUES(12,3,1000,2);
+INSERT INTO pledges VALUES(13,5,4000,4);
+INSERT INTO pledges VALUES(14,6,4000,5);
+INSERT INTO pledges VALUES(15,7,3000,6);
+INSERT INTO pledges VALUES(16,8,2000,6);
+INSERT INTO pledges VALUES(17,9,1000,7);
+INSERT INTO pledges VALUES(18,2,5000,7);
+INSERT INTO pledges VALUES(19,2,1000,8);
+INSERT INTO pledges VALUES(20,1,500,2);
+INSERT INTO pledges VALUES(21,1,2000,1);
+INSERT INTO pledges VALUES(22,3,1000,2);
+INSERT INTO pledges VALUES(23,5,4000,4);
+INSERT INTO pledges VALUES(24,6,4000,5);
+INSERT INTO pledges VALUES(25,7,3000,6);
+INSERT INTO pledges VALUES(26,8,2000,6);
+INSERT INTO pledges VALUES(27,9,1000,7);
+INSERT INTO pledges VALUES(28,2,5000,7);
+INSERT INTO pledges VALUES(29,2,1000,8);
+INSERT INTO pledges VALUES(30,1,500,2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
