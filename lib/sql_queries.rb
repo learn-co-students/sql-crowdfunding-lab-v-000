a3e6590e62766db0cd097c903102d9ea87b1ae6a
Jonathan Stone <jon_p_stone@yahoo.com>
2016-06-15 22:32:42 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,25 +7,25 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts
-"Write your SQL query Here"
+"SELECT projects.title, pledge.amount FROM projects INNER JOIN pledge ON projects.id = pledge.project_id"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges
-"Write your SQL query Here"
+"SELECT users.name, users.age, pledge.amount FROM users INNER JOIN pledge ON users.id = pledge.user_id"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+"SELECT projects.title, pledge.amount FROM projects INNER JOIN pledge ON projects.id = pledge.project_id WHERE pledge.amount >= projects.funding_goal"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount
-"Write your SQL query Here"
+"SELECT users.name, pledge.amount FROM users INNER JOIN pledge ON users.id = pledge.user_id GROUP BY users.name ORDER BY pledge.amount DESC"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"SELECT projects.category, pledge.amount FROM projects INNER JOIN pledge ON projects.id = pledge.project_id WHERE projects.category = 'music'"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_book_category
-"Write your SQL query Here"
+"SELECT projects.category, pledge.amount SUM(pleadge.amount) FROM projects INNER JOIN pledge ON projects.id = pledge.project_id WHERE projects.category = 'music'"
 end
