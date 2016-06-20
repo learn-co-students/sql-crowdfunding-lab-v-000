d329323f7602123e1bca271ee7254aa215725fc3
Jason <jasonkwong11@gmail.com>
2016-06-20 15:55:20 -0400

    done

---
+++ lib/insert.sql
@@ -0,0 +1,190 @@
+INSERT INTO users (name, age)
+  VALUES ("Alshon Jeffrey", 26);
+
+INSERT INTO users (name, age)
+  VALUES ("Elon Musk", 43);
+
+INSERT INTO users (name, age)
+  VALUES ("Lebron James", 31);
+
+INSERT INTO users (name, age)
+  VALUES ("Kyrie Irving", 24);
+
+INSERT INTO users (name, age)
+  VALUES ("JR Smith", 28);
+
+INSERT INTO users (name, age)
+  VALUES ("Tristan Thompson", 27);
+
+INSERT INTO users (name, age)
+  VALUES ("Kevin Love", 26);
+
+INSERT INTO users (name, age)
+  VALUES ("Steve Jobs", 67);
+
+INSERT INTO users (name, age)
+  VALUES ("Jeff Bezos", 55);
+
+INSERT INTO users (name, age)
+  VALUES ("Larry Page", 47);
+
+INSERT INTO users (name, age)
+  VALUES ("Malcolm X", 42);
+
+INSERT INTO users (name, age)
+  VALUES ("Martin Luther King", 31);
+
+INSERT INTO users (name, age)
+  VALUES ("Kanye West", 38);
+
+INSERT INTO users (name, age)
+  VALUES ("Jay-Z", 55);
+
+INSERT INTO users (name, age)
+  VALUES ("Bruce Wayne", 30);
+
+INSERT INTO users (name, age)
+  VALUES ("Tony Stark", 48);
+
+INSERT INTO users (name, age)
+  VALUES ("Johnny Depp", 53);
+
+INSERT INTO users (name, age)
+  VALUES ("Chris Evans", 35);   
+
+INSERT INTO users (name, age)
+  VALUES ("Scarlett Johanssen", 32); 
+
+INSERT INTO users (name, age)
+  VALUES ("Alfred Hitchcock", 88); 
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+  VALUES ("SpaceX", "Aerospace", 95000, 2009, 2011);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+  VALUES ("Tesla", "Automotive", 5500, 2003, 2007);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+  VALUES ("Paypal", "Banking", 232000, 2001, 2003);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+  VALUES ("NASA", "Aerospace", 67000, 2004, 2005);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+  VALUES ("Amazon", "Retail", 14000, 2002, 2004);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+  VALUES ("Google", "Tech", 99000, 2001, 2002);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+  VALUES ("Boeing", "Flight", 33000, 2004, 2011);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+  VALUES ("Airbus", "Flight", 4400, 2002, 2007);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+  VALUES ("Ford", "Automotive", 23400, 2003, 2009);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+  VALUES ("Chevy", "Automotive", 6600, 2003, 2006);
+
+INSERT INTO pledges (amount, user_id, project_id)
+  VALUES (33000, 17, 8);
+
+INSERT INTO pledges (amount, user_id, project_id)
+  VALUES (2000, 13, 4);
+
+INSERT INTO pledges (amount, user_id, project_id)
+  VALUES (43100, 11, 2);
+
+INSERT INTO pledges (amount, user_id, project_id)
+  VALUES (2300, 2, 4);
+
+INSERT INTO pledges (amount, user_id, project_id)
+  VALUES (1700, 13, 4);
+
+INSERT INTO pledges (amount, user_id, project_id)
+  VALUES (4400, 3, 7);
+
+INSERT INTO pledges (amount, user_id, project_id)
+  VALUES (21450, 19, 1);
+
+INSERT INTO pledges (amount, user_id, project_id)
+  VALUES (100, 20, 3);
+
+INSERT INTO pledges (amount, user_id, project_id)
+  VALUES (770, 14, 6);
+
+INSERT INTO pledges (amount, user_id, project_id)
+  VALUES (28790, 18, 5);
+
+INSERT INTO pledges (amount, user_id, project_id)
+  VALUES (9390, 3, 7);
+
+INSERT INTO pledges (amount, user_id, project_id)
+  VALUES (424200, 12, 6);
+
+INSERT INTO pledges (amount, user_id, project_id)
+  VALUES (93930, 16, 4);
+
+INSERT INTO pledges (amount, user_id, project_id)
+  VALUES (3210, 12, 1);
+
+INSERT INTO pledges (amount, user_id, project_id)
+  VALUES (1300, 2, 10);  
+
+INSERT INTO pledges (amount, user_id, project_id)
+  VALUES (948200, 3, 10);
+
+INSERT INTO pledges (amount, user_id, project_id)
+  VALUES (424200, 12, 6);
+
+INSERT INTO pledges (amount, user_id, project_id)
+  VALUES (93930, 16, 4);
+
+INSERT INTO pledges (amount, user_id, project_id)
+  VALUES (3210, 12, 1);
+
+INSERT INTO pledges (amount, user_id, project_id)
+  VALUES (1300, 2, 10);  
+
+INSERT INTO pledges (amount, user_id, project_id)
+  VALUES (948200, 3, 10);
+
+
+INSERT INTO pledges (amount, user_id, project_id)
+  VALUES (2000, 13, 4);
+
+INSERT INTO pledges (amount, user_id, project_id)
+  VALUES (43100, 11, 2);
+
+INSERT INTO pledges (amount, user_id, project_id)
+  VALUES (2300, 2, 4);
+
+INSERT INTO pledges (amount, user_id, project_id)
+  VALUES (1700, 13, 4);
+
+INSERT INTO pledges (amount, user_id, project_id)
+  VALUES (4400, 3, 7);
+
+INSERT INTO pledges (amount, user_id, project_id)
+  VALUES (21450, 19, 1);
+
+INSERT INTO pledges (amount, user_id, project_id)
+  VALUES (100, 20, 3);
+
+INSERT INTO pledges (amount, user_id, project_id)
+  VALUES (33000, 17, 8);
+
+INSERT INTO pledges (amount, user_id, project_id)
+  VALUES (2000, 13, 4);
+
+
+
+
+
+
+
+
+
+
