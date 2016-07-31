500f50268c6bafb32149aabf03a70acf48c2bc2c
nadukrow <nadukrow@gmail.com>
2016-07-31 16:55:05 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (name, age)
+  VALUES ("Albus", 113), ("Alex", 33), ("Amanda", 24), ("Bear", 6), ("Ena", 24), ("Finnebar", 17), ("Franz", 100), ("Hermione", 30), ("Iguana", 4), ("Katie", 24), ("Marisa", 24), ("Pacha", 5), ("Rosey", 9), ("Sirius", 36), ("Sophie", 24), ("Squid", 5), ("Swizzle", 4), ("Victoria", 23), ("Voldemort", 90), ("Whale", 6);
+
+INSERT INTO projects (title)
+  VALUES ("Animal shelter needs dog food"), ("Help me buy a guitar"), ("Help save birds of paradise"), ("I have bed bugs!"), ("I want to teach English in China"), ("Iguana needs tail operation"), ("My book on SQL"), ("The next Harry Potter"), ("The next Inna-Gadda-Davida"), ("Voldement needs a body");
+
+INSERT INTO pledges (amount)
+  VALUES (470),(20),(40),(50),(100),(70),(90),(50),(10),(170),(24),(60),(270),(12),(1700),(34),(125.5),(12),(13),(12),(12),(12),(12),(12),(12),(12),(12),(12),(12),(12);
