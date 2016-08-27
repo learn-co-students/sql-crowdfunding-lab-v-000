f40b0205846c04c1ddc9061d7797373eec43d41a
christinechoi <smi77y@gmail.com>
2016-08-26 20:19:41 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,145 @@
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("first", "water", 1000, 101016, 101017);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("second", "poverty", 2000, 111116, 111118);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ('third', 'energy', 4500, 013117, 013119);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ('fourth', 'education', 10000, 122516, 123119);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ('fifth', 'poverty', 7500, 030117, 091518);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ('sixth', 'energy', 5000, 072316, 090118);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ('seventh', 'poverty', 30000, 090917, 123120);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("eighth", 'water', 1000, 101016, 110117);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("ninth", 'education', 7500, 120116, 060117);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ('tenth', 'energy', 9500, 120116, 120118);
+--
+
+INSERT INTO users (name, age)
+VALUES ('James', 25);
+INSERT INTO users (name, age)
+VALUES ('John', 55);
+INSERT INTO users (name, age)
+VALUES ("Patricia", 34);
+INSERT INTO users (name, age)
+VALUES ('Jennifer', 31);
+INSERT INTO users (name, age)
+VALUES ('Robert', 45);
+INSERT INTO users (name, age)
+VALUES ('Michael', 43);
+INSERT INTO users (name, age)
+VALUES ('Elizabeth', 22);
+INSERT INTO users (name, age)
+VALUES ('Linda', 65);
+INSERT INTO users (name, age)
+VALUES ('William', 55);
+INSERT INTO users (name, age)
+VALUES ('David', 67);
+INSERT INTO users (name, age)
+VALUES ('Barbara', 39);
+INSERT INTO users (name, age)
+VALUES ('Susan', 47);
+INSERT INTO users (name, age)
+VALUES ('David', 37);
+INSERT INTO users (name, age)
+VALUES ('Richard', 21);
+INSERT INTO users (name, age)
+VALUES ('Jessica', 29);
+INSERT INTO users (name, age)
+VALUES ("Margaret", 28);
+INSERT INTO users (name, age)
+VALUES ('Joseph', 51);
+INSERT INTO users (name, age)
+VALUES ('Thomas', 38);
+INSERT INTO users (name, age)
+VALUES ('Sarah', 33);
+INSERT INTO users (name, age)
+VALUES ('Karen', 46);
+--
+
+INSERT INTO pledges (amount)
+VALUES (10);
+
+INSERT INTO pledges (amount)
+VALUES (25);
+
+INSERT INTO pledges (amount)
+VALUES (34.5);
+INSERT INTO pledges (amount)
+VALUES (60);
+INSERT INTO pledges (amount)
+VALUES (5);
+INSERT INTO pledges (amount)
+VALUES (150);
+INSERT INTO pledges (amount)
+VALUES (32);
+INSERT INTO pledges (amount)
+VALUES (15);
+INSERT INTO pledges (amount)
+VALUES (400);
+INSERT INTO pledges (amount)
+VALUES (75);
+
+INSERT INTO pledges (amount)
+VALUES (50);
+
+INSERT INTO pledges (amount)
+VALUES (25);
+
+INSERT INTO pledges (amount)
+VALUES (25);
+INSERT INTO pledges (amount)
+VALUES (25);
+INSERT INTO pledges (amount)
+VALUES (25);
+INSERT INTO pledges (amount)
+VALUES (50);
+INSERT INTO pledges (amount)
+VALUES (200);
+INSERT INTO pledges (amount)
+VALUES (75);
+INSERT INTO pledges (amount)
+VALUES (10);
+INSERT INTO pledges (amount)
+VALUES (25);
+
+INSERT INTO pledges (amount)
+VALUES (50);
+
+INSERT INTO pledges (amount)
+VALUES (50);
+
+INSERT INTO pledges (amount)
+VALUES (35);
+INSERT INTO pledges (amount)
+VALUES (15);
+INSERT INTO pledges (amount)
+VALUES (80);
+INSERT INTO pledges (amount)
+VALUES (100);
+INSERT INTO pledges (amount)
+VALUES (250);
+INSERT INTO pledges (amount)
+VALUES (45);
+INSERT INTO pledges (amount)
+VALUES (35);
+INSERT INTO pledges (amount)
+VALUES (75);
+
+
+
