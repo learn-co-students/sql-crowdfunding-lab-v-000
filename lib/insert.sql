6200dc0b2cc8fb8b211fcb8ce949bbeb81509cf4
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-09-25 17:19:32 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,65 @@
+INSERT INTO users (name, age) VALUES ('tom', 35);
+INSERT INTO users (name, age) VALUES ('roger', 44);
+INSERT INTO users (name, age) VALUES ('dan', 11);
+INSERT INTO users (name, age)VALUES ('sadya' , 55);
+INSERT INTO users (name, age) VALUES ('sam', 77);
+INSERT INTO users (name, age)VALUES ('top', 33);
+INSERT INTO users (name, age)VALUES ('tracy', 65);
+INSERT INTO users (name, age)VALUES ('tracy', 65);
+INSERT INTO users (name, age)VALUES ('tracy', 65);
+INSERT INTO users (name, age)VALUES ('tracy', 65);
+INSERT INTO users (name, age)VALUES ('tracy', 65);
+INSERT INTO users (name, age)VALUES ('tracy', 65);
+INSERT INTO users (name, age)VALUES ('tracy', 65);
+INSERT INTO users (name, age)VALUES ('tracy', 65);
+INSERT INTO users (name, age)VALUES ('tracy', 65);
+INSERT INTO users (name, age)VALUES ('tracy', 65);
+INSERT INTO users (name, age)VALUES ('tracy', 65);
+INSERT INTO users (name, age)VALUES ('tracy', 65);
+INSERT INTO users (name, age) VALUES ('tracy', 65);
+INSERT INTO users (name, age) VALUES ('tracy', 65);
+
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ( 'IPAD SPEAKER', 'electronics', 67000, 'october 7th 2015' , 'june 15th 2018');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ( 'IPAD SPEAKER', 'electronics', 67000, 'october 7th 2015' , 'june 15th 2018');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ( 'IPAD SPEAKER', 'electronics', 67000, 'october 7th 2015' , 'june 15th 2018');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ( 'IPAD SPEAKER', 'electronics', 67000, 'october 7th 2015' , 'june 15th 2018');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ( 'IPAD SPEAKER', 'electronics', 67000, 'october 7th 2015' , 'june 15th 2018');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ( 'IPAD SPEAKER', 'electronics', 67000, 'october 7th 2015' , 'june 15th 2018');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ( 'IPAD SPEAKER', 'electronics', 67000, 'october 7th 2015' , 'june 15th 2018');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ( 'IPAD SPEAKER', 'electronics', 67000, 'october 7th 2015' , 'june 15th 2018');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ( 'IPAD SPEAKER', 'electronics', 67000, 'october 7th 2015' , 'june 15th 2018');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ( 'IPAD SPEAKER', 'electronics', 67000, 'october 7th 2015' , 'june 15th 2018');
+
+
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (5550, 8, 55);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (3328, 23, 21);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2223, 5, 66);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2223, 5, 66);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2223, 5, 66);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2223, 5, 66);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2223, 5, 66);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2223, 5, 66);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2223, 5, 66);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2223, 5, 66);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2223, 5, 66);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2223, 5, 66);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2223, 5, 66);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2223, 5, 66);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2223, 5, 66);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2223, 5, 66);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2223, 5, 66);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2223, 5, 66);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2223, 5, 66);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2223, 5, 66);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2223, 5, 66);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2223, 5, 66);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2223, 5, 66);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2223, 5, 66);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2223, 5, 66);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2223, 5, 66);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2223, 5, 66);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2223, 5, 66);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2223, 5, 66);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2223, 5, 66);
