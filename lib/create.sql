30c137ab18f6655396d4523a1a20533d6a980fba
John Verdone <jmverdone@gmail.com>
2016-10-01 19:04:23 -0400

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,22 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age Integer
+);
+
+
+CREATE TABLE projects (
+    id INTEGER PRIMARY KEY,
+    category TEXT,
+    title TEXT,
+    funding_goal INTEGER,
+    start_date TEXT,
+    end_date TEXT
+);
+
+CREATE TABLE pledges (
+  id INTEGER PRIMARY KEY,
+  amount INTEGER,
+  user_id INTEGER,
+  project_id INTEGER
+);
