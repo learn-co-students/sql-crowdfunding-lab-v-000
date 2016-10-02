e1865a2cb181bdbfd38ad044e131e618a57fdd94
Clare <clare.e.horton@gmail.com>
2016-10-02 04:36:20 +0000

    2 of 6 queries

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,25 +7,31 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts
-    "SELECT projects.title, SUM(pledges.amount) FROM pledges INNER JOIN projects ON pledges.project_id = projects.id GROUP BY projects.title;"
+    "SELECT projects.title, SUM(pledges.amount) 
+    FROM pledges 
+    INNER JOIN projects ON pledges.project_id = projects.id 
+    GROUP BY projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges
-"Write your SQL query Here"
+    "SELECT users.name, users.age, SUM(pledges.amount)
+    FROM users
+    INNER JOIN pledges ON users.id = pledges.user_id
+    GROUP BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+    "Write your SQL query Here"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount
-"Write your SQL query Here"
+    "Write your SQL query Here"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+    "Write your SQL query Here"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_book_category
-"Write your SQL query Here"
+    "Write your SQL query Here"
 end
