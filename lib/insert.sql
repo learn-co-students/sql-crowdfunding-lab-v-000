a6f3e84d08ad33593592242cd17ec3194dffdfeb
Clare <clare.e.horton@gmail.com>
2016-10-02 03:57:21 +0000

    added projects

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+("The First Project", "first", 1000000, "August 2017", "September 2017"),
+("Another Project", "not-first", 3000, "March 2017", "September 2017"),
+("The Projects Project", "first", 46000, "May 2015", "October 2014"),
+("The Fourth Project", "not-first", 4000, "September 2016", "October 2016"),
+("I'm Tired Of Thinking Of Project Names Now", "void", 1500, "tomorrow", "yesterday"),
+("The Void Stared Back", "voidless", 600000000000, "next week", "today"),
+("Donald Trump Might Be Voldemort", "antagonistic", 666, "now", "in a moment"),
+("The Librarians Have Escaped", "nightvale", 5000, "in the mists", "of time"),
+("What Number Am I On Now Only Nine Shoot", "endless", 89999, "endless", "forever"),
+("The Last Project", "thank-deity", 9000, "just now", "it's over");
