5dd1e166ee770d87ac684763098dbc4729cfb13f
Janine Rosen <janinemrosen@gmail.com>
2016-10-21 00:35:08 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,24 @@
+#projects
+CREATE TABLE projects(
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date TEXT,
+  end_date TEXT
+);
+
+#users
+CREATE TABLE users(
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
+
+#pledges
+CREATE TABLE pledges(
+  id INTEGER PRIMARY KEY,
+  amount INTEGER,
+  user_id INTEGER,
+  project_id INTEGER
+);
