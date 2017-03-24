9a057e4f390b2751d33330455dec5e43a9dc0196
David Lewitan <dovidlwtn@gmail.com>
2017-03-24 02:58:19 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,15 +19,28 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges
  FROM Users
    INNER JOIN Pledges
      ON Pledges.user_id = Users.id
-   GROUP BY Users.name"
+   GROUP BY Users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+"SELECT projects.title,
+        SUM(pledges.amount) - projects.funding_goal AS pledge_sum
+ FROM projects
+   INNER JOIN pledges
+     ON pledges.project_id = projects.id
+ GROUP BY projects.title
+   HAVING pledge_sum >= 0;"
+
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount
-"Write your SQL query Here"
+"SELECT users.name,
+        SUM(pledges.amount) as pledge_sum
+ FROM users
+ INNER JOIN pledges
+   ON pledges.user_id = users.id
+ GROUP BY users.name
+ ORDER BY pledge_sum ASC;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
