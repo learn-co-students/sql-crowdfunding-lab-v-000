d187c4ea95efccb17970592dcb5da9a051878667
Aditi Green <asaki444@gmail.com>
2017-05-03 02:39:47 +0000

    Done.

--- lib/create.sql
+++ lib/create.sql
@@ -1,3 +1,4 @@
 CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
 CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date TEXT, end_date TEXT);
 CREATE TABLE pledges (id INTEGER PRIMARY KEY, user_id TEXT, project_id TEXT, amount INTEGER);
+ 
