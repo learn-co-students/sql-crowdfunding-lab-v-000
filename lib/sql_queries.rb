ee63855167b6d02619c587aec608e149d59f8894
Daniela Carvajal <d.carvajal.agama@gmail.com>
2017-05-13 03:55:11 +0000

    done sql-crowfunding lab

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,31 +19,16 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  #JOIN pledges ON projects.id = pledges.project_id
-  #(projects.funding_goal - pledges.amount) AS pledges.amount
-  "SELECT projects.title, pledges.amount FROM projects
-  JOIN pledges ON projects.funding_goal = pledges.amount
-  WHERE projects.funding_goal <= pledges.amount
-  GROUP BY projects.title;"
-  #valores mas pequeños
-  #JOIN pledges ON projects.funding_goal = pledges.amount
-  #fecha final ?
-  # sume las donaciones y compare con el monto meta
-  #primero sumar el monto de las donaciones por ID del proyectos
-  #comparo monto meta - monto si el resultado es mayor que 0 aún no acaba
-  # si es igual a cero ya acabó el proyecto
-
-  #valor meta, cantidad que donaron, booleans 0-1v
+  "SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) AS amount_left FROM projects
+  JOIN pledges ON projects.id = pledges.project_id
+  GROUP BY projects.title HAVING amount_left >= 0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount
   "SELECT users.name, SUM(pledges.amount) FROM users
   INNER JOIN pledges ON users.id = pledges.user_id
   GROUP BY users.name
-  ORDER BY MAX(pledges.amount) ASC;
-  "
-  #ID de usuario ordenar en desc
-  #rango
+  ORDER BY SUM(pledges.amount);"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
