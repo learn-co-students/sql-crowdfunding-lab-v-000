496e1408b1bd6ff596e5af02beb1fe49004f1b5e
curlywallst <nnoyes@comcast.net>
2017-05-29 18:46:08 -0400

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,8 +1,8 @@
 # Write your sql queries in this file in the appropriate method like the example below:
 #
-# def select_category_from_projects
-# "SELECT category FROM projects;"
-# end
+def select_category_from_projects
+"SELECT category FROM projects;"
+end
 
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
