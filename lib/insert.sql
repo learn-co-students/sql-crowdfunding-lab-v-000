e7ba1112a8e1882e250540da822b66bff018dadd
curlywallst <nnoyes@comcast.net>
2017-05-29 20:08:36 -0400

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -7,7 +7,7 @@ INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUE
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Pay for Term 4", "Tuition", 30000, "2016-01-01", "2019-01-15");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Pay for Term 5", "Tuition", 30000, "2017-01-01", "2019-07-15");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Pay for Term 6", "Tuition", 30000, "2017-01-01", "2020-01-01");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("PBuy Computer", "School Supplies",4000, "2016-05-01", "2017-07-15");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Buy Computer", "School Supplies",4000, "2016-05-01", "2017-07-15");
 
 INSERT INTO users (name, age) VALUES ("Mom", 59);
 INSERT INTO users (name, age) VALUES ("Aunt Linda", 61);
@@ -50,7 +50,7 @@ INSERT INTO pledges (amount, user_id, project_id) VALUES (3000,20,10);
 INSERT INTO pledges (amount, user_id, project_id) VALUES (1500,14,7);
 INSERT INTO pledges (amount, user_id, project_id) VALUES (200,19,8);
 INSERT INTO pledges (amount, user_id, project_id) VALUES (250,5,1);
-INSERT INTO pledges (amount, user_id, project_id) VALUES (10000,69);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (10000,6,9);
 INSERT INTO pledges (amount, user_id, project_id) VALUES (1000,17,3);
 INSERT INTO pledges (amount, user_id, project_id) VALUES (100,12,3);
 INSERT INTO pledges (amount, user_id, project_id) VALUES (750,15,3);
@@ -61,4 +61,3 @@ INSERT INTO pledges (amount, user_id, project_id) VALUES (25,11,2);
 INSERT INTO pledges (amount, user_id, project_id) VALUES (30,10,2);
 INSERT INTO pledges (amount, user_id, project_id) VALUES (10000,6,8);
 INSERT INTO pledges (amount, user_id, project_id) VALUES (7500,7,9);
-INSERT INTO pledges (amount, user_id, project_id) VALUES (6000,7,9);
