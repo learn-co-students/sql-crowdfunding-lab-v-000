a982835fd0f9dc643593a6fbec411becdb95285a
Andrew <github email address>
2017-06-12 11:06:01 -0400

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,25 @@
+CREATE TABLE projects (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    category TEXT,
+    funding_goal NUMERIC,
+    start_date INTEGER,
+    end_date INTEGER
+
+
+);
+
+CREATE TABLE users(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER
+
+
+);
+CREATE TABLE pledges(
+    id INTEGER PRIMARY KEY,
+    amount INTEGER,
+    User_id INTEGER,
+    project_id INTEGER
+
+);
