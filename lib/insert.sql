b4cebb8e40a20c0cf2ae12d23e1d5ec54d25f6a8
Noah Summers <summers.noah@gmail.com>
2017-07-02 11:26:14 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,132 @@
+INSERT INTO users (name, age) VALUES
+('Ryder', 38),
+('Willow', 70),
+('Delilah', 22),
+('Nathaniel', 34),
+('Alyssa', 26),
+('Luca', 60),
+('Blake', 55),
+('Hadley', 68),
+('Adalynn', 19),
+('Robert', 52),
+('Everett', 53),
+('Rylee', 29),
+('Alexis', 44),
+('Camden', 46),
+('Brielle', 28),
+('Micah', 19),
+('King', 19),
+('Melanie', 76),
+('Sara', 68),
+('Weston', 68);
+
+INSERT INTO projects
+(title, category, funding_goal, start_date, end_date)
+VALUES
+('PROJECT SNOWFLAKE', 'Ice Cream Flavors',   66000, '2017-07-13', '2018-01-31'),
+('PROJECT GISPERT',   'Spices',              6500,  '2017-07-15', '2018-02-03'),
+('PROJECT LAGER',     'Things With Tails',   27000, '2017-07-31', '2018-05-01'),
+('PROJECT LAGUNA',    'Terms of Endearment', 15000, '2017-08-16', '2018-06-05'),
+('PROJECT GEMME',     'Breakfast Food',      77000, '2017-08-17', '2018-07-12'),
+('PROJECT SNOWMASS',  'Types of Drinks',     2900,  '2017-08-29', '2018-07-14'),
+('PROJECT ELOISE',    'Places to Hang Out',  29000, '2017-09-02', '2018-07-20'),
+('PROJECT MARIE',     'Medicine',            33000, '2017-09-19', '2018-08-03'),
+('PROJECT REDLION',   'Cars',                32000, '2017-10-16', '2018-09-25'),
+('PROJECT GREEK',     'Beers',               1600,  '2017-10-26', '2018-11-10');
+
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES
+(100, 13, 5),
+(63, 6, 4),
+(89, 18, 7),
+(96, 13, 4),
+(7, 15, 3),
+(38, 14, 6),
+(71, 5, 8),
+(47, 1, 9),
+(56, 13, 8),
+(93, 17, 9),
+(17, 1, 2),
+(75, 17, 2),
+(93, 11, 3),
+(60, 15, 2),
+(94, 1, 7),
+(69, 2, 8),
+(39, 16, 9),
+(55, 9, 9),
+(51, 20, 9),
+(42, 15, 1),
+(65, 8, 8),
+(41, 8, 6),
+(67, 7, 7),
+(17, 2, 7),
+(75, 19, 1),
+(28, 17, 4),
+(44, 4, 7),
+(86, 19, 2),
+(100, 1, 10),
+(76, 4, 5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
