6cc759abd7cfd6be46b2653d7d41d2eca0ca4d1b
Rebecca Bratu <beckybratu@gmail.com>
2017-08-04 03:57:57 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,179 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+  VALUES ("Amabrush", "product design", 700000, 070117, 080117);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+  VALUES ("Passion Planner", "product design", 200000, 072017, 090117);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+  VALUES ("Sound Blocker", "gadgets", 20000, 073017, 093017);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+  VALUES ("Ticwatch", "wearables", 200000, 050217, 062917);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+  VALUES ("The Family Tree", "books", 10000, 042017, 052717);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+  VALUES ("Ocean Beats", "music", 50000, 021417, 042017);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+  VALUES ("Sous Vide", "technology", 250000, 080117, 122017);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+  VALUES ("Re:Legend", "video games", 100000, 062417, 083017);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+  VALUES ("Drawing Blood", "books", 70000, 072817, 102317);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+  VALUES ("Toilet Night Light", "gadgets", 5000, 052717, 073017);
+
+INSERT INTO users (name, age)
+  VALUES ("Anja Rubik", 31);
+
+INSERT INTO users (name, age)
+  VALUES ("Doutzen Kroes", 30);
+
+INSERT INTO users (name, age)
+  VALUES ("Karlie Kloss", 25);
+
+INSERT INTO users (name, age)
+  VALUES ("Gigi Hadid", 22);
+
+INSERT INTO users (name, age)
+  VALUES ("Bella Hadid", 21);
+
+INSERT INTO users (name, age)
+  VALUES ("Kendall Jenner", 21);
+
+INSERT INTO users (name, age)
+  VALUES ("Kylie Jenner", 20);
+
+INSERT INTO users (name, age)
+  VALUES ("Naomi Campbell", 44);
+
+INSERT INTO users (name, age)
+  VALUES ("Kate Moss", 46);
+
+INSERT INTO users (name, age)
+  VALUES ("Jourdan Dunn", 24);
+
+INSERT INTO users (name, age)
+  VALUES ("Cara Delevigne", 25);
+
+INSERT INTO users (name, age)
+  VALUES ("Joan Smalls", 26);
+
+INSERT INTO users (name, age)
+  VALUES ("Anne V", 27);
+
+INSERT INTO users (name, age)
+  VALUES ("Isabeli Fontana", 33);
+
+INSERT INTO users (name, age)
+  VALUES ("Izabel Goulart", 30);
+
+INSERT INTO users (name, age)
+  VALUES ("Adriana Lima", 34);
+
+INSERT INTO users (name, age)
+  VALUES ("Andreea Diaconu", 28);
+
+INSERT INTO users (name, age)
+  VALUES ("Kendall Walters", 22);
+
+INSERT INTO users (name, age)
+  VALUES ("Bianca Balti", 25);
+
+INSERT INTO users (name, age)
+  VALUES ("Bar Rafaeli", 32);
+
+INSERT INTO pledges (amount, project_id, user_id)
+  VALUES (50, 2, 12);
+
+INSERT INTO pledges (amount, project_id, user_id)
+  VALUES (100, 3, 10);
+
+INSERT INTO pledges (amount, project_id, user_id)
+  VALUES (250, 8, 11);
+
+INSERT INTO pledges (amount, project_id, user_id)
+  VALUES (25, 7, 14);
+
+INSERT INTO pledges (amount, project_id, user_id)
+  VALUES (100, 5, 12);
+
+INSERT INTO pledges (amount, project_id, user_id)
+  VALUES (60, 3, 20);
+
+INSERT INTO pledges (amount, project_id, user_id)
+  VALUES (100, 5, 15);
+
+INSERT INTO pledges (amount, project_id, user_id)
+  VALUES (250, 6, 12);
+
+INSERT INTO pledges (amount, project_id, user_id)
+  VALUES (40, 9, 16);
+
+INSERT INTO pledges (amount, project_id, user_id)
+  VALUES (150, 10, 19);
+
+INSERT INTO pledges (amount, project_id, user_id)
+  VALUES (1000, 8, 17);
+
+INSERT INTO pledges (amount, project_id, user_id)
+  VALUES (800, 3, 9);
+
+INSERT INTO pledges (amount, project_id, user_id)
+  VALUES (300, 2, 18);
+
+INSERT INTO pledges (amount, project_id, user_id)
+  VALUES (120, 1, 7);
+
+INSERT INTO pledges (amount, project_id, user_id)
+  VALUES (250, 8, 13);
+
+INSERT INTO pledges (amount, project_id, user_id)
+  VALUES (450, 9, 19);
+
+INSERT INTO pledges (amount, project_id, user_id)
+  VALUES (320, 4, 18);
+
+INSERT INTO pledges (amount, project_id, user_id)
+  VALUES (642, 3, 20);
+
+INSERT INTO pledges (amount, project_id, user_id)
+  VALUES (200, 5, 10);
+
+INSERT INTO pledges (amount, project_id, user_id)
+  VALUES (1200, 10, 15);
+
+INSERT INTO pledges (amount, project_id, user_id)
+  VALUES (900, 1, 11);
+
+INSERT INTO pledges (amount, project_id, user_id)
+  VALUES (2000, 7, 19);
+
+INSERT INTO pledges (amount, project_id, user_id)
+  VALUES (10000, 9, 20);
+
+INSERT INTO pledges (amount, project_id, user_id)
+  VALUES (25000, 1, 13);
+
+INSERT INTO pledges (amount, project_id, user_id)
+  VALUES (13000, 7, 11);
+
+INSERT INTO pledges (amount, project_id, user_id)
+  VALUES (11000, 10, 2);
+
+INSERT INTO pledges (amount, project_id, user_id)
+  VALUES (6500, 1, 1);
+
+INSERT INTO pledges (amount, project_id, user_id)
+  VALUES (4000, 1, 20);
+
+INSERT INTO pledges (amount, project_id, user_id)
+  VALUES (2500, 3, 9);
+
+INSERT INTO pledges (amount, project_id, user_id)
+  VALUES (3000, 5, 16);
