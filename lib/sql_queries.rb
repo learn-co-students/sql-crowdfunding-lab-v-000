27e919e60f6ea917a85f384487ba4396c45c6167
Mason Ellwood <masonellwood@gmail.com>
2017-08-09 11:29:10 -0700

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,25 +7,65 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"Write your SQL query Here"
+  <<-SQL
+    select projects.title, sum(pledges.amount)
+    from projects
+    join pledges
+    on projects.id = pledges.project_id
+    group by projects.title
+  SQL
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+  <<-SQL
+    select users.name, users.age, sum(pledges.amount)
+    from users
+    join pledges
+    on users.id = pledges.user_id
+    group by users.name
+
+  SQL
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+  <<-SQL
+    select projects.title, (sum(pledges.amount) - projects.funding_goal) as amount_over
+    from projects
+    join pledges
+    on projects.id = pledges.project_id
+    group by projects.title
+    having amount_over >= 0
+  SQL
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+  <<-SQL
+    select users.name, sum(pledges.amount) as something
+    from users
+    join pledges
+    on users.id = pledges.user_id
+    group by users.name
+    order by something asc
+  SQL
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+  <<-SQL
+    select projects.category, pledges.amount
+    from projects
+    join pledges
+    on projects.id = pledges.project_id
+    where projects.category = 'music'
+  SQL
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"Write your SQL query Here"
+  <<-SQL
+    select projects.category, sum(pledges.amount)
+    from projects
+    join pledges
+    on projects.id = pledges.project_id
+    group by projects.category
+    having category = 'books'
+  SQL
 end
