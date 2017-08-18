b78ed19a7357bb77410bd08146481f1dcd77d5d3
Terry-Thompson <terry.lyn.thompson@gmail.com>
2017-08-18 14:29:09 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,71 @@
+INSERT INTO users (name, age) VALUES 
+("Bruce Wayne", 40),
+("Clark Kent", 20),
+("Diana Prince", 25),
+("James Howlett", 30),
+("Pyotr Nikolayevich Rasputin", 50),
+("Ororo Munroe", 18),
+("Wade Winston Wilson", 23),
+("Kurt Wagner", 21),
+("Vincent Mesmero", 42),
+("Warren Kenneth Worthington III", 35),
+("Henry McCoy", 41),
+("Scott Summers", 23),
+("Jean Grey", 22),
+("Remy LeBeau", 19),
+("Arkady Rossovich", 42),
+("Erik Magnus Lehnsherr", 52),
+("Tony Stark", 31),
+("Mega Man", 18),
+("Bruce Banner", 25),
+("Simon Belmont", 17);
+
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+("Gotham Enema", "Crime Fighting", 12000, "Immediately", "Criminals jailed"),
+("Shutdown Mayhem", "Crime Fighting", 200000, "July 5th, 2018", "July 12th, 2018"),
+("Genetic Research", "Scientific Inquiry", 2000000, "January 1st, 2018", "December 31st, 2018"),
+("World Domination", "Mayhem", 1000000000, "August 21, 2018", "July 4, 2030"),
+("Poker Tournament", "Leisure", 20000, "May 5th, 2018", "May 8th, 2018"),
+("Meteorology Meeting", "Scientific Inquiry", 7000, "March 3rd, 2018", "June 3rd, 2018"),
+("Heavy Metal Concert", "Leisure", 10000, "June 1st, 2018", "June 2nd, 2018"),
+("Urban Demolition", "Mayhem", 6000, "April 1st, 2018", "April 1st, 2018"),
+("Optometry Union", "Scientific Inquiry", 40000, "May 10th, 2018", "May 11th, 2018"),
+("Super Suplex Seminar", "Leisure", 13000, "June 12th, 2018", "June 14th, 2018");
+
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES
+(12000, 1, 1),
+(300, 2, 1),
+(700, 3, 1),
+(100000, 18, 2),
+(100000, 17, 2),
+(100000, 8, 2),
+(50000, 13, 3),
+(1000000, 11, 3),
+(1000000, 19, 3),
+(500000000, 10, 4),
+(250000000, 9, 4),
+(250000000, 15, 4),
+(5, 7, 5),
+(10000, 14, 5),
+(10000, 13, 5),
+(2000, 11, 6),
+(2000, 6, 6),
+(3000, 5, 6),
+(3500, 16, 7),
+(3500, 5, 7),
+(3000, 20, 7),
+(2000, 15, 8),
+(2000, 16, 8),
+(2000, 9, 8),
+(20000, 12, 9),
+(10000, 11, 9),
+(10000, 2, 9),
+(4500, 19, 10),
+(4500, 5, 10),
+(4000, 11, 10);
+
+
+
+
