4f5ea5e46ba4e334475b6a5927eca336d08b6128
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-10-17 20:21:44 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,65 @@
+INSERT INTO users (name, age)VALUES ("Jerry",28);
+INSERT INTO users (name, age)VALUES ("Janet",60);
+INSERT INTO users (name, age)VALUES ("Lars",40);
+INSERT INTO users (name, age)VALUES ("Lisa",24);
+INSERT INTO users (name, age)VALUES ("Mike",26);
+INSERT INTO users (name, age)VALUES ("Michelle",24);
+INSERT INTO users (name, age)VALUES ("Nathan",41);
+INSERT INTO users (name, age)VALUES ("Naomi",30);
+INSERT INTO users (name, age)VALUES ("Avi", 28);
+INSERT INTO users (name, age)VALUES ("Amanda",41);
+INSERT INTO users (name, age)VALUES ("Sam",31);
+INSERT INTO users (name, age)VALUES ("Sara",19);
+INSERT INTO users (name, age)VALUES ("Brent",21);
+INSERT INTO users (name, age)VALUES ("Beatrice",21);
+INSERT INTO users (name, age)VALUES ("Beth",12);
+INSERT INTO users (name, age)VALUES ("Beyang",19);
+INSERT INTO users (name, age)VALUES ("Brian",19);
+INSERT INTO users (name, age)VALUES ("Bryan",15);
+INSERT INTO users (name, age)VALUES ("Beth",18);
+INSERT INTO users (name, age)VALUES ("Babs", 38);
+
+INSERT INTO Projects (title, category, funding_goal, start_date, end_date)
+VALUES
+("Life Project", "social", 1000000, "4/14/1988", "4/28/2030"),
+("Life Project", "social", 1000000, "4/14/1988", "4/28/2030"),
+("Life Project", "social", 1000000, "4/14/1988", "4/28/2030"),
+("Life Project", "social", 1000000, "4/14/1988", "4/28/2030"),
+("Life Project", "social", 1000000, "4/14/1988", "4/28/2030"),
+("Life Project", "social", 1000000, "4/14/1988", "4/28/2030"),
+("Life Project", "social", 1000000, "4/14/1988", "4/28/2030"),
+("Life Project", "social", 1000000, "4/14/1988", "4/28/2030"),
+("Life Project", "social", 1000000, "4/14/1988", "4/28/2030"),
+("Life Project", "social", 1000000, "4/14/1988", "4/28/2030");
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES
+(1000, 1, 3),
+(1000, 1, 3),
+(1000, 1, 3),
+(1000, 1, 3),
+(1000, 1, 3),
+(1000, 1, 3),
+(1000, 1, 3),
+(1000, 1, 3),
+(1000, 1, 3),
+(1000, 1, 3),
+(1000, 1, 3),
+(1000, 1, 3),
+(1000, 1, 3),
+(1000, 1, 3),
+(1000, 1, 3),
+(1000, 1, 3),
+(1000, 1, 3),
+(1000, 1, 3),
+(1000, 1, 3),
+(1000, 1, 3),
+(1000, 1, 3),
+(1000, 1, 3),
+(1000, 1, 3),
+(1000, 1, 3),
+(1000, 1, 3),
+(1000, 1, 3),
+(1000, 1, 3),
+(1000, 1, 3),
+(1000, 1, 3),
+(1000, 1, 3);
