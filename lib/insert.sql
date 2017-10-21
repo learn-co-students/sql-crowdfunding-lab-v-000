3e6a758ffc3c74d7a917f8a70268dd5e6001cba3
Denis Bondin <dsbondin@gmail.com>
2017-10-21 16:30:14 +0000

    insert.sql

---
+++ lib/insert.sql
@@ -0,0 +1,33 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+("Bakery", "offline", 20000, 10/01/2017, 12/31/2017),
+("Liquor Store", "offline", 100000, 9/01/2017, 12/01/2017),
+("Casino", "online", 24000, 10/10/2017, 11/31/2017),
+("Cafe", "offline", 36000, 11/01/2017, 12/31/2017),
+("Dating Website", "online", 9000, 9/01/2017, 11/31/2017),
+("Spinner Manufacturing", "offline", 45000, 10/15/2017, 12/15/2017),
+("Shoes Store", "online", 39000, 10/01/2017, 12/05/2017),
+("Gadget Store", "online", 19000, 10/31/2017, 12/31/2017),
+("Jewelry Store", "nline", 120000, 08/01/2017, 12/01/2017),
+("Charging Station", "offline", 200000, 10/21/2017, 4/31/2018);
+
+INSERT INTO users (name, age) VALUES
+("Denis", 37),
+("Tanya", 35),
+("Aya", 7),
+("Bill Gates", 57),
+("Donald Trump", 69),
+("Sanek", 36),
+("Pashok", 37),
+("Misha", 35),
+("Ju", 37),
+("Mr Robot", 27),
+("Denis", 37),
+("Tanya", 35),
+("Aya", 7),
+("Bill Gates", 57),
+("Donald Trump", 69),
+("Sanek", 36),
+("Pashok", 37),
+("Misha", 35),
+("Ju", 37),
+("Mr Robot", 27);
