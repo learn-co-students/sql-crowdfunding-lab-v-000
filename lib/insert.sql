6a03e298749492821ca6ee3b6cfc74e81bbf8b52
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-10-24 20:53:13 +0000

    halfway there

---
+++ lib/insert.sql
@@ -0,0 +1,64 @@
+INSERT INTO users VALUES (1, "Ben", 22);
+INSERT INTO users VALUES (2, "Alan", 35);
+INSERT INTO users VALUES (3, "Boomer", 39);
+INSERT INTO users VALUES (4, "Alex", 47);
+INSERT INTO users VALUES (5, "Jim", 13);
+INSERT INTO users VALUES (6, "James", 45);
+INSERT INTO users VALUES (7, "John", 59);
+INSERT INTO users VALUES (8, "Alice", 19);
+INSERT INTO users VALUES (9, "Beth", 22);
+INSERT INTO users VALUES (10, "Ava", 89);
+INSERT INTO users VALUES (11, "Bertha", 34);
+INSERT INTO users VALUES (12, "Alaina", 46);
+INSERT INTO users VALUES (13, "Jenny", 53);
+INSERT INTO users VALUES (14, "Jackie", 31);
+INSERT INTO users VALUES (15, "Jo", 32);
+INSERT INTO users VALUES (16, "Zena", 65);
+INSERT INTO users VALUES (17, "Kindria", 19);
+INSERT INTO users VALUES (18, "Lucy", 28);
+INSERT INTO users VALUES (19, "Penny", 27);
+INSERT INTO users VALUES (20, "Hon", 26);
+
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
+(1, 'Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30'),
+(2, 'My book on SQL', 'books', 20.00, '2013-01-01', '2014-01-01'),
+(3, 'The next Harry Potter', 'books', 1000.00, '2013-05-30', '2015-05-30'),
+(4, 'Animal shelter needs dog food', 'charity', 400.00, '2013-03-14', '2013-06-30'),
+(5, 'Voldement needs a body', 'charity', 6000.00, '2013-03-20', '2013-09-20'),
+(6, 'The next Inna-Gadda-Davida', 'music', 200.00, '2014-12-30', '2015-12-30'),
+(7, 'Iguana needs tail operation', 'charity', 2000.00, '2013-10-02', '2013-10-30'),
+(8, 'I have bed bugs!', 'charity', 800.00, '2014-06-30', '2014-06-31'),
+(9, 'I want to teach English in China', 'charity', 3000.00, '2013-06-30', '2013-09-30'),
+(10, 'Help save birds of paradise', 'charity', 5000.00, '2012-03-20', '2013-06-30');
+
+INSERT INTO pledges VALUES
+  (1, 230, 9, 7),
+  (2, 120, 18, 6),
+  (3, 50, 1, 2),
+  (4, 10, 8, 10),
+  (5, 15, 4, 3),
+  (6, 80, 16, 7),
+  (7, 58, 20, 7),
+  (8, 87, 4, 8),
+  (9, 97, 18, 8),
+  (10, 38, 4, 3),
+  (11, 97,17,7),
+  (12, 90, 3, 6),
+  (13, 40, 6, 3),
+  (14, 61, 1, 4),
+  (15, 100,8,10),
+  (16,92,20,6),
+  (17,22, 14, 7),
+  (18, 140, 16, 6),
+  (19, 168, 18, 7),
+  (20, 197, 6,3),
+  (21, 164,18,1),
+  (22,161,15,1),
+  (23,166,1,9),
+  (24, 134, 17,3),
+  (25, 100, 4, 5),
+  (26, 189, 2, 9),
+  (27, 130, 5, 10),
+  (28, 120, 8, 2),
+  (29, 198, 12, 3),
+  (30, 140, 15, 1);  
