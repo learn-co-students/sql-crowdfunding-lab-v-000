01002cb0268b365068d58d91f14d523521e6d022
Miriam Peskowitz <miriam5000@gmail.com>
2017-10-31 20:14:28 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,23 @@
+CREATE TABLE project (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT
+  funding_goal INTEGER,
+  start_date TEXT,
+  end_date TEXT,
+);
+
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE pledge (
+  id INTEGER PRIMARY KEY,
+  amount INTEGER,
+  user_id TEXT,
+  project_id TEXT
+);
+
+# belongs to user and to project --> thi smeans create user_id and project_id inside that table.
