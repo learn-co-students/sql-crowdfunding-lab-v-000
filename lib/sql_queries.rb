077213cc16d40c1bd9b6ac8cf7a596b93c067800
Susan Fowler <infopro94@gmail.com>
2017-11-04 22:09:33 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,15 +7,18 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"Write your SQL query Here"
+  "SELECT projects.title, SUM(pledges.amount) FROM projects JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title;"
+  #select project titles and their pledge ammoiunts. Alphabetize by project title. Unclear that pledges had to be added.
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+  "SELECT users.name, users.age, SUM(pledges.amount) FROM users JOIN pledges ON users.id = pledges.user_id GROUP BY users.name;"
+  #select users.name, users.age, pledges.amount for all pledges, sort alphabetically by name. Join users and pledges tables.
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+  "SELECT projects.title, SUM(pledges.amount) - projects.funding_goal AS amount_left FROM projects JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title HAVING amount_left >= 0;"
+  #select projects that have met funding goal, select those that exceeded funding goal, list with project titles. Math required.
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
