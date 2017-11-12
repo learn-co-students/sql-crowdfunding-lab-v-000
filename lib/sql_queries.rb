233c61e7c65dd2a61849ddff53e746fdaff8a6c9
Chris Roberts <chrisrobertspdx@gmail.com>
2017-11-12 17:33:02 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,13 +19,13 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+"select u.name, sum(pl.amount) as amount from pledges pl inner join users u on u.id = pl.user_id group by u.name order by amount, u.name"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"select p.category, pl.amount from projects p inner join pledges pl on p.id = pl.project_id and p.category = 'music'"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"Write your SQL query Here"
+"select p.category, sum(pl.amount) as amount from projects p inner join pledges pl on p.id = pl.project_id and p.category = 'books' group by p.category order by amount desc, p.category"
 end
