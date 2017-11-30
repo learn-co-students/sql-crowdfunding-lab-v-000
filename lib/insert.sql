cab1886198134aa268f14da58f12d664f3476db9
Olena Ageyeva <eageeva@hotmail.com>
2017-11-30 02:30:51 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,34 @@
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
+(1,"Project1", "Category1", "Goal1", 01/01/2017, 12/31/2017),
+(2,"Project2", "Category1", "Goal1", 01/01/2017, 12/31/2017),
+(3,"Project3", "Category1", "Goal1", 01/01/2017, 12/31/2017),
+(4,"Project4", "Category1", "Goal1", 01/01/2017, 12/31/2017),
+(5,"Project5", "Category1", "Goal1", 01/01/2017, 12/31/2017),
+(6,"Project6", "Category1", "Goal1", 01/01/2017, 12/31/2017),
+(7,"Project7", "Category1", "Goal1", 01/01/2017, 12/31/2017),
+(8,"Project8", "Category1", "Goal1", 01/01/2017, 12/31/2017),
+(9,"Project9", "Category1", "Goal1", 01/01/2017, 12/31/2017),
+(10,"Project10", "Category1", "Goal1", 01/01/2017, 12/31/2017);
+
+INSERT INTO users (id, name, age) VALUES
+(1,"Name1", 21),
+(2,"Name2", 22),
+(3,"Name3", 23),
+(4,"Name4", 24),
+(5,"Name5", 25),
+(6,"Name6", 26),
+(7,"Name7", 27),
+(8,"Name8", 28),
+(9,"Name9", 29),
+(10,"Name10", 30),
+(11,"Name10", 31),
+(12,"Name10", 32),
+(13,"Name10", 33),
+(14,"Name10", 34),
+(15,"Name10", 35),
+(16,"Name10", 36),
+(17,"Name10", 37),
+(18,"Name10", 38),
+(19,"Name10", 39),
+(20,"Name10", 40)
+;
