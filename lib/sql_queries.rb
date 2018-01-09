2641de3ce076e6d4c5941ccd12c46ac2f940f759
Parker Catalano <catalano.work@gmail.com>
2018-01-09 21:14:36 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,23 +7,23 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"SELECT projects.title, SUM(pledges.amount) FROM projects LEFT JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title ORDER BY(title) ASC"
+"SELECT projects.title, SUM(pledges.amount) FROM projects LEFT JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title ORDER BY(title) ASC;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"SELECT users.name, SUM(pledges.amount) FROM users JOIN pledges ON users.id = pledges.user_id GROUP BY users.name ORDER BY(name) ASC"
+"SELECT users.name, users.age, SUM(pledges.amount) FROM users JOIN pledges ON users.id = pledges.user_id GROUP BY users.name ORDER BY(name) ASC;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+"SELECT projects.title, SUM(pledges.amount) - projects.funding_goal AS amount_over_or_equal_to_goal FROM projects JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title HAVING SUM(pledges.amount) - projects.funding_goal >= 0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+"SELECT users.name, SUM(pledges.amount) FROM users JOIN pledges ON users.id = pledges.user_id GROUP BY users.name ORDER BY SUM(pledges.amount), users.name"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"SELECT projects.category, users.name, pledges.amount FROM projects WHERE projects.category = 'music' JOIN pledges ON projects.id = pledges.project ID JOIN users ON pledges.user_id = users.id"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
