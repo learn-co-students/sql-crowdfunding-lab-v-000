127066f286abf4677cfc09da75aa8cf35ddad2d5
Blakeley Bond <burrick2003@yahoo.com>
2018-01-10 03:54:38 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,16 @@
+CREATE TABLE bears
+    (id INTEGER PRIMARY KEY,
+    title TEXT,
+    category TEXT,
+    goal INTEGER,
+    start_date TEXT,
+    end_date TEXT);
+CREATE TABLE projects
+  (
+
+  );
+CREATE TABLE users
+  (
+
+  );
+CREATE TABLE pledges
