8f75bbe297c6ceb0852455ef7549c7d4fa1c7597
Blakeley Bond <burrick2003@yahoo.com>
2018-01-10 23:52:27 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+    VALUES (1, "Mr. Chocolate", 1, "M", "brown", "playful", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+    VALUES (1, "Mr. Chocolate", 1, "M", "brown", "playful", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+    VALUES (1, "Mr. Chocolate", 1, "M", "brown", "playful", 1);
