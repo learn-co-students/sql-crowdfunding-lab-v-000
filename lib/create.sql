f1c5d6796f2cb9ba8cb0dee118ccfd3377973eeb
charityyoungblood <youngblood.charity@gmail.com>
2018-01-10 19:10:24 -0800

    added categories to projects table in create.sql

--- lib/create.sql
+++ lib/create.sql
@@ -1,7 +1,9 @@
 CREATE TABLE projects (
   id INTEGER PRIMARY KEY, 
-
-
-
-
+    title TEXT, 
+    category TEXT, 
+    funding_goal INTEGER,
+    start_date TEXT, # format will be "MM-DD-YY" 
+    end_date TEXT, # format will be "MM-DD-YY"
 );
+
