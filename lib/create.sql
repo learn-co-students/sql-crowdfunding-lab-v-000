e6e4709fc5e85c637e1fda6ffcf7d6e24265c925
charityyoungblood <youngblood.charity@gmail.com>
2018-01-11 11:07:50 -0800

    testing SUM function in sql_queries.rb

--- lib/create.sql
+++ lib/create.sql
@@ -2,9 +2,10 @@ CREATE TABLE projects (
   id INTEGER PRIMARY KEY, 
     title TEXT, 
     category TEXT, 
-    funding_goal INTEGER,
+    funding_goal NUMERIC,
     start_date TEXT,  
     end_date TEXT 
+      
 );
 
 CREATE TABLE users (
@@ -15,7 +16,8 @@ CREATE TABLE users (
 
 CREATE TABLE pledges (
   id INTEGER PRIMARY KEY, 
-    amount INTEGER, 
+    amount NUMERIC, 
     user_id INTEGER, 
-    project_id INTEGER
+    project_id INTEGER,
+      FOREIGN KEY (project_id) REFERENCES projects(project_id)
   ); 
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,7 +7,10 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"Write your SQL query Here"
+  # the SUM of pledge amounts based on project_id
+  # select the amounts where projects.id = pledges.project_id
+  "SELECT SUM(amount) FROM pledges WHERE project_id = 3"
+#"SELECT TOTAL(amount) FROM projects, pledges WHERE projects.id = 1"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
