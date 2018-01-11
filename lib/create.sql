0b72c7eb1fb4ad0e00539a9056f704176ebca6f2
charityyoungblood <youngblood.charity@gmail.com>
2018-01-10 19:12:03 -0800

    created users table in create.sql

--- lib/create.sql
+++ lib/create.sql
@@ -4,6 +4,11 @@ CREATE TABLE projects (
     category TEXT, 
     funding_goal INTEGER,
     start_date TEXT, # format will be "MM-DD-YY" 
-    end_date TEXT, # format will be "MM-DD-YY"
+    end_date TEXT # format will be "MM-DD-YY"
 );
 
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY, 
+    name TEXT, 
+    age INTEGER 
+); 
