a26e09674fb729418e82b37893eb54b647aa8368
Rygel-XVI <micha232@umn.edu>
2018-01-21 01:17:17 -0800

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("one", "tech", 10000, "2-2-16", "2-4-17"), ("one", "tech", 10000, "2-2-16", "2-4-17"), ("one", "tech", 10000, "2-2-16", "2-4-17"), ("one", "tech", 10000, "2-2-16", "2-4-17"), ("one", "tech", 10000, "2-2-16", "2-4-17");
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("one", "tech", 10000, "2-2-16", "2-4-17"), ("one", "tech", 10000, "2-2-16", "2-4-17"), ("one", "tech", 10000, "2-2-16", "2-4-17"), ("one", "tech", 10000, "2-2-16", "2-4-17"), ("one", "tech", 10000, "2-2-16", "2-4-17");
+
+INSERT INTO users (name, age) VALUES ("name", 20), ("name", 20), ("name", 20), ("name", 20), ("name", 20), ("name", 20), ("name", 20), ("name", 20), ("name", 20), ("name", 20);
+
+INSERT INTO users (name, age) VALUES ("name", 20), ("name", 20), ("name", 20), ("name", 20), ("name", 20), ("name", 20), ("name", 20), ("name", 20), ("name", 20), ("name", 20);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (28, 3, 100), (28, 3, 100), (28, 3, 100), (28, 3, 100), (28, 3, 100), (28, 3, 100), (28, 3, 100), (28, 3, 100), (28, 3, 100), (28, 3, 100), (28, 3, 100), (28, 3, 100), (28, 3, 100), (28, 3, 100), (28, 3, 100);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (28, 3, 100), (28, 3, 100), (28, 3, 100), (28, 3, 100), (28, 3, 100), (28, 3, 100), (28, 3, 100), (28, 3, 100), (28, 3, 100), (28, 3, 100), (28, 3, 100), (28, 3, 100), (28, 3, 100), (28, 3, 100), (28, 3, 100);
