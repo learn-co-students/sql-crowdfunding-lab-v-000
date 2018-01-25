0b473eb0470ac7b54f9f8c5aa503f4adb0fce0fc
David Towster <djt4xe@virginia.edu>
2018-01-24 17:54:44 -0800

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -52,13 +52,11 @@ WHERE projects.category = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-
-
-end
-
 "SELECT projects.category, SUM(pledges.amount)
 FROM pledges
 LEFT JOIN projects
 ON pledges.project_id = projects.id
 WHERE projects.category = 'books'
 ;"
+
+end
