8419e06aa807c908affdc14505f2ef0ca2cd5d68
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-01-29 15:18:42 -0500

    Articulated and stubbed out RandomEntryGenerator interface. Articulated SQLComposer interface. Stubbed out bin file

--- bin/random_entry_generator.rb
+++ bin/random_entry_generator.rb
@@ -2,4 +2,28 @@
 
 require_relative 'environment'
 
-RandomEntryGenerator.test
+RandomEntryGenerator.users(20)
+RandomEntryGenerator.projects(10)
+RandomEntryGenerator.pledges(30)
+
+=begin
+target_file = File.new('../lib/insert.sql', 'w')
+
+target_file.write(SQLComposer.compose(
+    SQLComposer::INSERT, 
+    RandomEntryGenerator.users(20), 
+    'users'
+)
+target_file.write(SQLComposer.compose(
+    SQLComposer::INSERT, 
+    RandomEntryGenerator.projecs(10), 
+    'projects'
+)
+target_file.write(SQLComposer.compose(
+    SQLComposer::INSERT, 
+    RandomEntryGenerator.pledges(30), 
+    'pledges'
+)
+
+target_file.close
+=end
--- lib/RandomEntryGenerator.rb
+++ lib/RandomEntryGenerator.rb
@@ -2,4 +2,16 @@ class RandomEntryGenerator
     def self.test
         puts "Hello! I am the RandomEntryGenerator class!"
     end
+
+    def self.users(number)
+        puts "Generating #{number} users!"
+    end
+
+    def self.projects(number)
+        puts "Generating #{number} projects!"
+    end
+
+    def self.pledges(number)
+        puts "Generating #{number} pledges!"
+    end
 end
