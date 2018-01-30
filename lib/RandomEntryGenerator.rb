2bfd086a967e56afbe61b9e2ab1078150c9b3bf0
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-01-29 19:24:12 -0500

    Implemented RandomEntryGenerator.users and the helper random_in_range(min, max, increment = nil) function

--- lib/RandomEntryGenerator.rb
+++ lib/RandomEntryGenerator.rb
@@ -1,10 +1,24 @@
 class RandomEntryGenerator
+    USER_MIN_AGE  = 8
+    USER_MAX_AGE = 48
+    USER_NAMES = ['Router', 'Yuki', 'Marley', 'Ilya', 'Dan', 'Patrick', 'Serge', 'Lenny', 'Peter', 'Darby', 'Edgar']
+    
+
     def self.test
         puts "Hello! I am the RandomEntryGenerator class!"
     end
 
     def self.users(number)
-        puts "Generating #{number} users!"
+        @@users = number.times.collect.with_index(1) do |value, index|
+            {
+                :id => index,
+                :name => USER_NAMES.sample,
+                :age => random_in_range(USER_MIN_AGE, USER_MAX_AGE)
+            }
+        end
+
+        puts "Generated #{number} users! Here they are: \n"\
+            "#{@@users}"
     end
 
     def self.projects(number)
@@ -13,5 +27,13 @@ class RandomEntryGenerator
 
     def self.pledges(number)
         puts "Generating #{number} pledges!"
+    end
+
+    def self.random_in_range(min, max, increment = nil)
+        increment = (increment ? increment : 1)
+
+        steps = ((max - min) / increment).to_i
+
+        (Random.rand(steps + 1) * increment) + min
     end
 end
