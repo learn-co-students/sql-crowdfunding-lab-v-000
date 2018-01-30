a81dda8592d77ff4e5a65b42d70c8167a8abcfbc
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-01-29 21:01:59 -0500

    Corrected SQLComposer.insert to correctly quote string data values. Passed all insert.sql tests

--- lib/SQLComposer.rb
+++ lib/SQLComposer.rb
@@ -2,7 +2,13 @@ class SQLComposer
     def self.insert(data_hashes_array, table_name)
         column_names_list = "(#{data_hashes_array.first.keys.join(', ')})"      
         column_values_lists = (data_hashes_array.collect do |data_hash|
-            "(#{data_hash.values.join(', ')})"
+            column_values_list = ""
+            column_values_list << "("
+            column_values_list <<
+                (data_hash.values.collect do |value|
+                    value.is_a?(String) ? "'#{value}'" : "#{value}"
+                end).join(', ')
+            column_values_list << ")"
         end).join(",\n")
 
         "INSERT INTO #{table_name} \n" \
