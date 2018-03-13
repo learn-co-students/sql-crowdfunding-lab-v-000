77ec7703bad71a30f5aef99b48c93dcfe5507465
Bryan Hooper <bryan@lindsayfarm.net>
2018-03-13 11:38:29 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO projects ( title, category, funding_goal, start_date, end_date ) 
+VALUES ("project alpha", "war", 100000, "01/01/1996", "03/01/1996"),
+("project tiger", "war", 21000, "03/03/1999", "04/09/1999"),
+("project killer", "war", 300040, "04/03/1976", "06/09/1978"),
+("project tester", "science", 304994, "03/04/2001", "04/05/2002"),
+('project dallas', "texas", 443332, "04/04/2002", "05/06/2004"),
+("project houston", "texas", 7079500, "09/25/1976", "10/14/1988"),
+("project austin", "texas", 9988554, "03/14/2010", "05/21/2011"),
+("project thomas", "oklahoma", 5599885, "05/20/2000", "08/12/2002"),
+("project lindsay", "oklahoma", 556674, "11/12/2001", "12/17/2010"),
+("project temple", "texas", 4433, "01/01/1999", "12/31/1999");
+
+INSERT INTO users ( name, age ) VALUES ("Able", 29), ("Bart", 28), ("Carl", 39), ("Doug", 33), ("Even", 24), ("Fred", 33), ("George", 44), ("Hansel", 45), ("Ian", 56), ("Jake", 25), ("Kevin", 32), ("Larry", 22), ("Moe", 33), ("Nancy", 25), ("Othello", 99), ("Percy", 54), ("Quinn", 55), ("Ralph", 23),("Steve", 32), ("Tom", 19);
+
+INSERT INTO pledges ( amount, user_id, project_id ) VALUES (1,1,1),(2,2,2 ),(3,3,3),(4,3,4),(5,4,5 ),(6,6,4),(7,6,6),(8,8,8),(9,9,9),(10,10,10),(11,11,10),(12,12,1),(13,13,2),(14,14,9),(15,20,4),(16,19,8),(17,18,6),(18,17,6),(19,16,5),(20,15,9),(21,14,3),(22,13,2),(23,9,4),(24,13,6),(25,15,6),(26,14,3),(27,15,8),(28,4,5),(29,5,7),(30,6,6);
+
+
