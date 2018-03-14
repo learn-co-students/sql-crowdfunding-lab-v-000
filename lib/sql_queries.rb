f20fbe1328fd764ed641713787ba5f541a60fcda
Ariana Berger <ariana.berger@gmail.com>
2018-03-14 20:46:25 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,15 +23,21 @@ GROUP BY users.name"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, " #I want to substract the total funding - goal and see if it's > 0
+"SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) AS goal_amount
+FROM projects
+INNER JOIN pledges
+ON projects.id = pledges.project_id
+GROUP BY projects.title
+HAVING goal_amount >= 0" #I want to substract the total funding - goal and see if it's > 0
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT users.name, sum(pledges.amount)
+"SELECT users.name, sum(pledges.amount) AS total_pledge
 FROM users
 INNER JOIN pledges
 ON users.id = pledges.user_id
-ORDER BY users.name" #why is it just giving me one entry with the total sum of the pledge amounts?
+GROUP BY users.name
+ORDER BY total_pledge"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
@@ -39,9 +45,15 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 FROM projects
 INNER JOIN pledges
 ON projects.id = pledges.project_id
-"
+WHERE projects.category = 'music'
+ORDER BY projects.category"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"Write your SQL query Here"
+  "SELECT projects.category, sum(pledges.amount)
+  FROM projects
+  INNER JOIN pledges
+  ON projects.id = pledges.project_id
+  WHERE projects.category = 'books'
+  ORDER BY projects.category"
 end
