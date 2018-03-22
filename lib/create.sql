89b070ba7058857ebd5a859e08947cc889e34568
Salma Elshahawy <salmaeng71@gmail.com>
2018-03-22 19:03:17 -0400

    Done.

--- lib/create.sql
+++ lib/create.sql
@@ -1,19 +1,21 @@
-CREATE TABLE project (
+CREATE TABLE projects (
   id INTEGER PRIMARY KEY,
   title TEXT,
   category TEXT,
-  funding goad INTEGER,
-  start_date INTEGER,
-  end_date INTEGER
+  funding_goal INTEGER,
+  start_date TEXT,
+  end_date TEXT
 );
 
-CREATE TABLE user (
+CREATE TABLE users (
   id INTEGER PRIMARY KEY,
   name TEXT,
-  age INTEGER,
+  age INTEGER
 );
 
-CREATE TABLE pledge (
+CREATE TABLE pledges (
   id INTEGER PRIMARY KEY,
   amount INTEGER,
+  user_id INTEGER,
+  project_id INTEGER
 );
