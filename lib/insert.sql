68868ecc78adb237926b0dbd87e414b188f3c831
ecssiah <ecssiah@gmail.com>
2018-04-25 03:48:39 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,71 @@
+insert into users 
+(name, age)
+values
+('Steven', 12),
+('Steven', 12),
+('Steven', 12),
+('Steven', 12),
+('Steven', 12),
+('Steven', 12),
+('Steven', 12),
+('Steven', 12),
+('Steven', 12),
+('Steven', 12),
+('Steven', 12),
+('Steven', 12),
+('Steven', 12),
+('Steven', 12),
+('Steven', 12),
+('Steven', 12),
+('Steven', 12),
+('Steven', 12),
+('Steven', 12),
+('Steven', 12);
+
+insert into projects 
+(title, category, funding_goal, start_date, end_date)
+values
+('The Impossible Project', 'Impossible', 100, '83', '18'),
+('The Impossible Project', 'Impossible', 100, '83', '18'),
+('The Impossible Project', 'Impossible', 100, '83', '18'),
+('The Impossible Project', 'Impossible', 100, '83', '18'),
+('The Impossible Project', 'Impossible', 100, '83', '18'),
+('The Impossible Project', 'Impossible', 100, '83', '18'),
+('The Impossible Project', 'Impossible', 100, '83', '18'),
+('The Impossible Project', 'Impossible', 100, '83', '18'),
+('The Impossible Project', 'Impossible', 100, '83', '18'),
+('The Impossible Project', 'Impossible', 100, '83', '18');
+
+insert into pledges
+(amount, user_id, project_id)
+values
+(10, 1, 1),
+(10, 1, 1),
+(10, 1, 1),
+(10, 1, 1),
+(10, 1, 1),
+(10, 1, 1),
+(10, 1, 1),
+(10, 1, 1),
+(10, 1, 1),
+(10, 1, 1),
+(10, 1, 1),
+(10, 1, 1),
+(10, 1, 1),
+(10, 1, 1),
+(10, 1, 1),
+(10, 1, 1),
+(10, 1, 1),
+(10, 1, 1),
+(10, 1, 1),
+(10, 1, 1),
+(10, 1, 1),
+(10, 1, 1),
+(10, 1, 1),
+(10, 1, 1),
+(10, 1, 1),
+(10, 1, 1),
+(10, 1, 1),
+(10, 1, 1),
+(10, 1, 1),
+(10, 1, 1);
