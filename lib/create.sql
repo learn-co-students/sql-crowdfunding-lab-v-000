2adb6aac061cbb0ef4e5fd2d969edd758d19b32e
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-05-17 13:19:17 -0700

    Done.

--- lib/create.sql
+++ lib/create.sql
@@ -23,23 +23,3 @@ CREATE TABLE pledges (
   user_id INTEGER,
   project_id INTEGER
 );
-
-
-
-
-
-
--- def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
---   "SELECT name, age, SUM(pledges.amount) FROM users
---   JOIN pledges
---   on users.id = pledges.user_id
---   GROUP BY users.name
---   ORDER BY users.name;"
--- end
---
--- def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
---   "SELECT title, SUM(pledges.amount)- projects.funding_goal FROM projects
---   JOIN pledges
---   ON pledges.project_id = projects.id
---   GROUP BY projects.title HAVING projects.funding_goal <= SUM(pledges.amount);"
--- end
