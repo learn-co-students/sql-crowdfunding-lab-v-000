02e3617f426e46871b95fa391df86dc63a4160e1
Brennan Fulmer <brennan.flmr@gmail.com>
2018-06-28 04:25:26 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -2,41 +2,41 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
   "SELECT projects.title, SUM(pledges.amount) 
   FROM projects
-  INNER JOIN pledges 
+  JOIN pledges 
   ON pledges.project_id = projects.id
   GROUP BY projects.title"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
   "SELECT users.name, users.age, SUM(pledges.amount)
-  FROM pledges
-  INNER JOIN users
+  FROM users
+  JOIN pledges
   ON pledges.user_id = users.id
   GROUP BY users.name"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT projects.title, SUM(pledges.amount) - projects.funding_goal
+  "SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) AS over_goal
   FROM projects
-  LEFT JOIN pledges
+  JOIN pledges
   ON pledges.project_id = projects.id
   GROUP BY projects.title
-  HAVING SUM(pledges.amount) >= projects.funding_goal"
+  HAVING over_goal >= 0; "
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-  "SELECT users.name, SUM(pledges.amount) AS total_pledged
+  "SELECT users.name, SUM(pledges.amount) AS total
   FROM users
-  LEFT OUTER JOIN pledges
+  JOIN pledges
   ON pledges.user_id = users.id
   GROUP BY users.name
-  ORDER BY total_pledged ASC, users.name"
+  ORDER BY total"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   "SELECT projects.category, pledges.amount 
-  FROM pledges
-  LEFT JOIN projects
+  FROM projects
+  JOIN pledges
   ON pledges.project_id = projects.id
   WHERE projects.category = 'music'; "
 end
@@ -44,7 +44,7 @@ end
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
   "SELECT projects.category, SUM(pledges.amount)
   FROM projects
-  LEFT JOIN pledges
+  JOIN pledges
   ON pledges.project_id = projects.id
   WHERE projects.category = 'books'; "
 end
