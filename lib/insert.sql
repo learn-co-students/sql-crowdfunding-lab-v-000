252c82657c798843c6fe98954dd794337155ee98
Heidi Engstrom <hengstrom25@gmail.com>
2018-07-06 15:19:54 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO projects (title) VALUES
+  ("Project 1"), ("Project 2"), ("Project 3"), ("Project 4"), ("Project 5"), ("Project 6"), ("Project 7"), ("Project 8"), ("Project 9"), ("Project 10");
+  
+INSERT INTO users (name) VALUES
+  ("Annie"), ("Bradley"), ("Chad"), ("David"), ("Erin"), ("Frederick"), ("Gene"), ("Heather"), ("Italo"), ("Jake"), ("Kelly"), ("Lana"), ("Michael"), ("Nathan"), ("Oscar"), ("Peggy"), ("Quentin"), ("Rachael"), ("Sally"), ("Tim");
+  
+INSERT INTO pledges (amount) VALUES
+(25), (50), (75), (100), (125), (150), (175), (200), (225), (250), (275), (300), (325), (350), (375), (400), (425), (450), (475), (500), (525), (550), (575), (600), (625), (650), (675), (700), (725), (750);
