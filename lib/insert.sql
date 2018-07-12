31a74d82200b2fae582fa6be4601f22210b1a22e
Nathan  Visak <nathan.visak@gmail.com>
2018-07-12 16:09:32 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,39 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Helping the homeless of NW Indiana", "charity", 10000, 07092018, 07012019);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Supporting local foster families", "charity", 5000, 07092018, 07092019);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Making a giant dinosaur costume out of cardboard", "fun/creativity", 500, 07092018, 09092018);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Special knives made out of REX 121", "commercial", 5000, 07102018, 09102018);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Supporting our local injured veterans", "charity", 10000, 07102018, 07102019);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Giving money to kids randomly to see what they do with it", "science experiments", 200, 07102018, 08102018);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Support for the Griffith middle school CC team", "charity", 2000, 07102018, 10102018);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Supporting the Griffith BlackCats basketball league", "charity", 5000, 07102018, 04102019);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Support for the Griffith middle school girls' volleyball team", "charity", 1000, 07102018, 10102018);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Dad's general fund for fixing broken things around the house", "charity", 5000, 07102018, 08102018);
+
+INSERT INTO users (name, age) VALUES ("Johnathan", 21);
+INSERT INTO users (name, age) VALUES ("Brandon", 49);
+INSERT INTO users (name, age) VALUES ("Keith", 34);
+INSERT INTO users (name, age) VALUES ("Anna", 36);
+INSERT INTO users (name, age) VALUES ("Nathan", 36);
+INSERT INTO users (name, age) VALUES ("Christina", 32);
+INSERT INTO users (name, age) VALUES ("Enrique", 32);
+INSERT INTO users (name, age) VALUES ("Casey", 36);
+INSERT INTO users (name, age) VALUES ("Linsday", 34);
+INSERT INTO users (name, age) VALUES ("Steve", 45);
+INSERT INTO users (name, age) VALUES ("Jason", 38);
+INSERT INTO users (name, age) VALUES ("Corey", 38);
+INSERT INTO users (name, age) VALUES ("Mike", 36);
+INSERT INTO users (name, age) VALUES ("Holly", 32);
+INSERT INTO users (name, age) VALUES ("Kristina", 39);
+INSERT INTO users (name, age) VALUES ("Tom", 39);
+INSERT INTO users (name, age) VALUES ("Bella", 13);
+INSERT INTO users (name, age) VALUES ("Ashley", 13);
+INSERT INTO users (name, age) VALUES ("Luke", 6);
+INSERT INTO users (name, age) VALUES ("Nate", 10);
+
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (20, 1, 1), (100, 2, 3), (200, 3, 4), (50, 6, 7), (70, 10, 10),
+(20, 1, 1), (40, 6, 7), (60, 12, 10), (80, 17, 5), (100, 22, 2),
+(20, 2, 2), (40, 8, 9), (60, 13, 9), (80, 18, 4), (100, 23, 3),
+(20, 3, 3), (40, 10, 1), (60, 14, 8), (80, 19, 3), (100, 24, 4),
+(20, 4, 4), (40, 2, 3), (60, 15, 7), (80, 20, 2), (100, 25, 5),
+(20, 5, 5), (40, 11, 3), (60, 16, 6), (80, 21, 1), (100, 26, 6);
