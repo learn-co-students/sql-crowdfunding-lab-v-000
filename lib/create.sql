f982ea41a8c2266c710ae6a8349eb1d44ff0f3e7
Nancy Jo <enciie@gmail.com>
2018-07-12 21:33:13 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,21 @@
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date NUMERIC,
+  end_date NUMERIC,
+);
+
+CREATE TABLE pledges (
+  id INTEGER PRIMARY KEY,
+  project_id INTEGER,
+  user_id INTEGER,
+  amount INTEGER
+);
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
