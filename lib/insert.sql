5159e2da805544478b699b4574d354004018c030
Marcus  = Soulo <souloinc87@gmail.com>
2018-08-23 07:50:15 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,57 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+("Save the wales", "animals", 10000, 08/01/2018, 08/28/2018);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+("Humanity", "Food", 20000, 06/01/2018, 07/01/2018);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+("Cancer awarness", "Health", 100000, 04/04/2018, 05/04/2018);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+("Stray animals", "Animals", 15000, 09/01/2018, 10/01/2018);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+("Cleaner air", "Environment", 30000, 03/01/2018, 04/01/2018);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+("Feed the Children", "Food", 40000, 01/01/2018, 02/01/2018);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+("Relay for life", "Health", 200000, 03/01/2018, 04/01/2018);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+("Clothes for tots", "Clothing", 25000, 11/01/2018, 12/01/2018);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+("Scholarships", "Education", 40000, 06/01/2018, 08/01/2018);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+("Cans for cans", "Food", 10000, 12/01/2018, 01/05/2019);
+
+INSERT INTO users (name, age) VALUES ("Wallie", 18), ("James", 19), ("Rose", 21),
+("Howard", 30), ("Janet", 25), ("Lensie", 26), ("Bob", 17), ("Rain", 16), ("Snow", 19),
+("Skip", 31), ("Neo", 11), ("Pete", 35), ("Allan", 29), ("Mark", 10), ("Sam", 15),
+("Carol", 20), ("Rachel", 17), ("Nomad", 27), ("Rocket", 19), ("Superman", 100);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES
+(50, 1, 4),
+(100, 2, 5),
+(75, 4, 6),
+(800, 9, 3),
+(1000, 11, 1),
+(100, 5, 2),
+(500, 3, 9),
+(5000, 15, 10),
+(2000, 19, 2),
+(700, 20, 1),
+(10, 12, 3),
+(1000, 18, 5),
+(6000, 17, 8),
+(1000, 20, 2),
+(100, 15, 3),
+(200, 16, 4),
+(400, 18, 5),
+(1100, 20, 8),
+(1200, 1, 1),
+(10, 10, 10),
+(1600, 13, 9),
+(25000, 12, 4),
+(1000, 7, 7),
+(50, 18, 3),
+(9000, 6, 5),
+(2100, 7, 9),
+(4400, 11, 10),
+(100, 12, 5),
+(1500, 15, 4),
+(3000, 18, 2);
