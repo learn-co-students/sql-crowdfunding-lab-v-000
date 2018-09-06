974a0d724cb78302335e958f524928aade7bbd87
Adam Clayman <adam@clayman.org>
2018-09-06 23:53:54 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,5 +27,5 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-  "SELECTS projects.category, SUM(pledges.amount) FROM Pledges LEFT OUTER JOIN Projects ON Projects.id = Pledges.project_id GROUP BY(projects.category) WHERE projects.category = 'books'"
+  "SELECTS projects.category, SUM(pledges.amount) FROM Pledges LEFT OUTER JOIN Projects ON Pledges.project_id = Projects.id GROUP BY(projects.category) WHERE projects.category = 'books'"
 end
