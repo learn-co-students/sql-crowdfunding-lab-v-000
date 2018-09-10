9937fb79e2763d6f1a9d3cce82a553b4e33d55b9
Heidi Hlavinka <hchlav@gmail.com>
2018-09-10 00:20:33 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (id, name, age) VALUES (1, "Maru", 3);
+-- 20
+
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (1, "Maru", 3, "Scottish Fold", 1000000);
+-- 10
+
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES (1, "Maru", 3, "Scottish Fold", 1000000);
+-- 30
