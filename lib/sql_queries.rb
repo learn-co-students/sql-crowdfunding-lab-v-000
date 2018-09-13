61debdb6755e44a987bca1b654fc8dbfbc06b035
Heidi Hlavinka <hchlav@gmail.com>
2018-09-13 21:56:36 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -26,15 +26,10 @@ GROUP BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, projects.funding_goal
+"SELECT projects.title, SUM(pledges.amount) - projects.funding_goal
 FROM projects
 INNER JOIN pledges
-;"
-
-# SELECT projects.title, projects.funding_goal, SUM(pledges.amount)
-# FROM projects
-# INNER JOIN pledges
-# GROUP BY pledges HAVING SUM(amount) > SUM(funding_goal);
+GROUP BY projects.title HAVING SUM(amount) > SUM(funding_goal);"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
