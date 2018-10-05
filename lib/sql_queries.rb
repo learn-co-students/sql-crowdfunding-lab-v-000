4c9d3b090b9de1d48d083144cbe2f1a4e5da2422
Mika88 <mikaspivak@gmail.com>
2018-10-05 19:21:21 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -10,16 +10,14 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 "SELECT projects.title, SUM(pledges.amount)
  FROM projects
  LEFT OUTER JOIN pledges ON projects.id = pledges.project_id
- GROUP BY pledges.project_id
- ORDER BY projects.title;"
+ GROUP BY projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
 "SELECT users.name, users.age, SUM(pledges.amount)
  FROM users
  INNER JOIN pledges ON users.id = pledges.user_id
- GROUP BY pledges.user_id
- ORDER BY users.name;"
+ GROUP BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
@@ -49,6 +47,6 @@ def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_b
 "SELECT projects.category, SUM(pledges.amount)
  FROM projects
  INNER JOIN pledges ON projects.id = pledges.project_id
- GROUP BY projects.category
- HAVING projects.category ='books';"
+ WHERE projects.category ='books'
+ GROUP BY projects.category;"
 end
