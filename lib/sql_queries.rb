5988f31517f46a7fdda46483751204d3b331b044
Harley Rayner <harleymteden@gmail.com>
2018-10-25 02:56:57 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,15 +15,31 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+"SELECT name, age, SUM(amount)
+ FROM users
+ INNER JOIN pledges
+ ON Users.id = Pledges.user_id
+ GROUP BY name
+ ORDER BY name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+"SELECT title, SUM(pledges.amount) - funding_goal
+ FROM projects
+ LEFT JOIN pledges
+ ON pledges.project_id = projects.id
+ GROUP BY projects.title
+ HAVING SUM(pledges.amount) - funding_goal >= 0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+"SELECT Users.name, SUM(Pledges.amount)
+ FROM Pledges
+ LEFT JOIN Users
+ ON Pledges.user_id = Users.id
+ GROUP BY Users.name
+ ORDER BY SUM(Pledges.amount)
+ "
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
