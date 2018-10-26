7f0a9d575878f5316324baf3e34744760ada4225
Amanda Murphy <helloamandamurphy@gmail.com>
2018-10-26 20:28:20 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -16,15 +16,18 @@ ORDER BY title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"SELECT users.name, users.age, pledge.amount 
-FROM pledges 
-JOIN 
-ON 
+"SELECT users.name, age, SUM(pledges.amount)
+FROM users 
+LEFT OUTER JOIN users
+ON users.id = pledges.user_id
 ORDER BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+"SELECT projects.title, pledges.amount
+FROM pledges
+INNER JOIN pledges
+ON projects."
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
