26a64e0753a0800acf9fcf6a981c16724745f06c
Amanda Murphy <helloamandamurphy@gmail.com>
2018-10-28 19:21:59 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -17,27 +17,37 @@ ORDER BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, SUM((pledges.amount)-projects.funding_goal)
+"SELECT projects.title, (SUM(pledges.amount)-projects.funding_goal)
+AS amount_left
 FROM projects
-LEFT OUTER JOIN pledges
+JOIN pledges
 ON projects.id = pledges.project_id
-GROUP BY projects.id
-WHERE SUM((pledges.amount)-projects.funding_goal) > 0"
+GROUP BY projects.title
+HAVING amount_left >= 0"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT users.name, pledges.amount
+"SELECT users.name, SUM(pledges.amount)
 FROM users
 INNER JOIN pledges
 ON users.id = pledges.user_id
-GROUP BY users.id
+GROUP BY users.name
 ORDER BY SUM(pledges.amount);"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"SELECT projects.category, pledges.amount
+FROM projects
+JOIN pledges
+ON projects.id = pledges.project_id
+WHERE projects.category = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"SELECT category, SUM(pledges.amount)"
+"SELECT projects.category, SUM(pledges.amount)
+FROM pledges
+LEFT OUTER JOIN projects
+ON projects.id = pledges.project_id
+WHERE projects.category = 'books'
+GROUP BY projects.category;"
 end
