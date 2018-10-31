70ba96c8a848c2769bedb3ed3c43d04381ae6fce
anihakutin <4hbrody@gmail.com>
2018-10-31 10:26:27 -0400

    Create.sql partialy done.

---
+++ lib/create.sql
@@ -0,0 +1,20 @@
+-- Create Tables
+-- In the create.sql file, model your tables. You should have a table for projects, users, and pledges.
+--
+-- Create a schema based on the following information:
+--
+-- A project has a title, a category, a funding goal, a start date, and an end date.
+-- A user has a name and an age
+-- A pledge has an amount. It belongs to a user, and it also belongs to a project.
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date INTEGER,
+  end_date INTEGER
+);
+
+CREATE TABLE user (
+  id 
+);
