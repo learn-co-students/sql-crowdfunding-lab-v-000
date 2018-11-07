d38ef0afaf37c402226e5060a530d2059cf71a9f
Azhar Shah <azharshahstl@yahoo.com>
2018-11-07 19:08:37 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Build an electric car', 'Vehicle', 1000000, 110218, 060119);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Snuber', 'Transportation', 50000, 111718, 123118);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Build a mouse', 'Crafts', 15000, 112118, 041119);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('BlueBook', 'Social Network', 10000000, 110118, 123119);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Wride', 'Transportation', 13000, 120108, 070119);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('TicTacDo', 'Games', 1000, 120518, 060119);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Electual Bike', 'Vehicle', 75000, 111118, 111119);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Flutter', 'Social Network', 150000, 111218, 010120);
+
