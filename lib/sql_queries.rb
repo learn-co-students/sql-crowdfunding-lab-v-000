2c386c1153565bd2da36024a604d66bf9a7d44bd
Azhar Shah <azharshahstl@yahoo.com>
2018-11-13 21:47:08 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,11 +15,11 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, "
+"SELECT projects.title, SUM (pledge.amount - projects.funding_goal) FROM projects ON u , "
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-""
+"SELECT users.name, SUM (pledges.amount) FROM users JOIN pledges ON users.id = pledges.user_id GROUP BY users.name ORDER BY SUM (pledges.amount), users.name;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
