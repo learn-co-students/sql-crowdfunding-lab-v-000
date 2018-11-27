7a68e080daeb89a6b30ffa31d17775db5abca534
Steven Crouse <sdcrouse91@gmail.com>
2018-11-27 09:11:06 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,35 @@
+INSERT INTO users (name, age) VALUES
+("Harry Potter", 38),
+("Steve Rogers", 50),
+("George Weasley", 40),
+("Taylor Swift", 28),
+("Alex Trebek", 78),
+("Peter Piper", 111),
+("SDC", 27),
+("Dick Van Dyke", 92),
+("Old King Cole", 200),
+("Jesus of Nazareth", 2022),
+("J.R.R. Tolkien", 88),
+("Frodo Baggins", 90),
+("Gandalf", 1000),
+("Julie Andrews", 83),
+("Mary Poppins", 84),
+("Hermione Granger", 39),
+("Emma Watson", 28),
+("Clint Eastwood", 88),
+("Cathy Guisewite", 68),
+("Mother Teresa", 108)
+;
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+("ZitBGone", "Cosmetics", 500431.50, "01/01/17", "05/05/20"),
+("Weasley's Wizarding Wheezes", "Pranks", 100000.75, "09/01/94", "12/25/97"),
+("Taylor Swift's Greatest Hits", "Music", 2000000.00, "11/27/18", "12/01/20"),
+("Top 100 Comic Strips Collection", "Book", 50000.50, "04/21/18", "10/21/18"),
+("Middle Earth: An Expected Journey", "Theme Park", 3333333.33, "07/29/54", "10/20/55"),
+("Healing the Sick", "Medicinal Outreach Program", 144000.00, "03/04/12", "07/10/21"),
+("Books4Kids", "Literacy Program", 210000.21, "04/05/70", "05/04/73"),
+("PATH (People Assisting The Homeless", "Homeless Outreach Program", 4500000.00, "03/15/10", "12/31/20"),
+("Wacky Wally's Wand Wepair", "Repair Shop", 12345.67, "09/19/94", "03/20/95"),
+("World Trade Center Museum", "Memorial Museum", 400000.40, "09/11/01", "05/02/11")
+;
