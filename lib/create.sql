997a3202ea4524eb1c154ce4569b325905da533c
Elias Ventura <eliasventura23@gmail.com>
2018-12-16 01:16:56 +0000

    Done.

--- lib/create.sql
+++ lib/create.sql
@@ -15,7 +15,7 @@ CREATE TABLE users (
 
 CREATE TABLE pledges (
   id INTEGER PRIMARY KEY,
-  user_id INTEGER,
   project_id INTEGER,
+  user_id INTEGER,
   amount INTEGER
 );
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,25 +7,25 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"SELECT title, SUM(pledges.amount) FROM projects JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title;";
+"SELECT title, SUM(amount) FROM projects JOIN pledges ON projects.id = pledges.project_id GROUP BY title ORDER BY title";
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"SELECT name, age, SUM(pledges.amount) FROM users JOIN pledges ON users.id = pledges.user_id GROUP BY users.name;"
+"SELECT name, age, SUM(amount) FROM users JOIN pledges on users.id = pledges.user_id GROUP BY name ORDER BY name";
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT title, SUM(pledges.amount) - projects.funding_goal FROM projects JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title HAVING SUM(pledges.amount) >= projects.funding_goal;"
+"SELECT title, (SUM(amount) - projects.funding_goal) AS amount_left FROM projects JOIN pledges ON projects.id = pledges.project_id GROUP BY title HAVING amount_left >= 0";
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT users.name, SUM(pledges.amount) FROM users JOIN pledges ON users.id = pledges.user_id GROUP BY users.name ORDER BY SUM(pledges.amount) ASC;"
+"SELECT name, SUM(amount) FROM users JOIN pledges ON users.id = pledges.user_id GROUP BY name ORDER BY SUM(amount)";
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"SELECT projects.category, pledges.amount FROM projects JOIN pledges ON  projects.id = pledges.project_id WHERE projects.category = 'music';"
+"SELECT category, amount FROM pledges JOIN projects ON pledges.project_id = projects.id WHERE category = 'music'";
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"SELECT projects.category, SUM(pledges.amount) FROM projects JOIN pledges on projects.id = pledges.project_id WHERE projects.category = 'books';"
+"SELECT category, SUM(amount) FROM projects JOIN pledges ON projects.id = pledges.project_id WHERE category = 'books'";
 end
