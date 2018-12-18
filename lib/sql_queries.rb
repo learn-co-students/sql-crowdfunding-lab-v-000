278299b49fd3bc8d64a0c2ffd68cfbf8344adefd
Karen Flores <karen.ag.flores@gmail.com>
2018-12-18 04:50:49 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,7 +20,7 @@ GROUP BY users.name"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, (projects.funding_goal - pledges.amount) FROM pledges
+"SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) FROM pledges
 JOIN projects ON projects.id = pledges.project_id
 GROUP BY projects.title HAVING SUM(pledges.amount) >= projects.funding_goal"
 end
