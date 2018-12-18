79e10623d90858514a4a769cfd01e05b17c732f3
Karen Flores <karen.ag.flores@gmail.com>
2018-12-18 04:06:33 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -42,6 +42,6 @@ ORDER BY projects.category
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"SELECT projects.category, SUM(pledges.amount) FROM pledges WHERE projects.category = 'books';
+"SELECT projects.category, SUM(pledges.amount) FROM pledges
 JOIN projects ON projects.id = pledges.project_id"
 end
