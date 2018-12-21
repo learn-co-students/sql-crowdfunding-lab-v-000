597b883136158a09ce52f3107247cd72575def14
github username <github email address>
2018-12-21 15:05:20 -0500

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -14,22 +14,31 @@ end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
 
-  "SELECT pledges.name, pledges.age, pledges.amount FROM pledges"
-
+  "SELECT users.name, users.age, SUM(pledges.amount) FROM users
+  JOIN pledges ON users.id = pledges.user_id
+  GROUP BY users.name;"
+# pledges >-- users
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+"SELECT projects.title,  (SUM(pledges.amount) - projects.funding_goal) AS amount_left FROM projects
+JOIN pledges ON  projects.id = pledges.project_id
+GROUP BY projects.title HAVING amount_left >= 0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+"SELECT users.name, pledges.amount FROM pledges
+JOIN users ON pledges.project_id = users.id
+GROUP BY users.name ORDER BY pledges.amount AND users.name;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+# "SELECT projects.category, pledges.amount FROM projects
+# JOIN pledges ON projects.id = pledges.project_id
+# "
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"Write your SQL query Here"
+# "SELECT projects.category, SUM(pledges.amount)
+# Write your SQL query Here"
 end
