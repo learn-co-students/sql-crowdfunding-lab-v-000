d39a47a9b54544e97f4c05ab04eb6e142aa36ffd
Ashley Wong <ashwong04@gmail.com>
2018-12-28 01:15:10 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -24,11 +24,11 @@ end
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
 "SELECT users.name, SUM(pledges.amount)
   FROM users LEFT JOIN pledges ON users.id = pledges.user_id
-  GROUP BY users.name;"
+  GROUP BY users.name ORDER BY SUM(pledges.amount);"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"SELECT "
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
