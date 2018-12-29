91209dfa08e7ab609a1e61953eaf74c4177ef127
Allison Howard <alliemhow@gmail.com>
2018-12-29 04:35:40 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,3 @@
-
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
 (1, 'Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30'),
 (2, 'My book on SQL', 'books', 20.00, '2013-01-01', '2014-01-01'),
