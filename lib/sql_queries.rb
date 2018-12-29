318809efaecf5c58edd930a312aef6d2407c1036
brennanmrose <brennan.m.rose@gmail.com>
2018-12-29 12:31:11 -0500

    completed lab

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,11 +1,3 @@
-# Write your sql queries in this file in the appropriate method like the example below:
-#
-# def select_category_from_projects
-# "SELECT category FROM projects;"
-# end
-
-# Make sure each ruby method returns a string containing a valid SQL statement.
-
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 	'SELECT pr.title, SUM(pl.amount) 
 	FROM projects pr
@@ -50,6 +42,3 @@ def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_b
 	JOIN pledges pl ON pr.id = pl.project_id
 	WHERE pr.category = "books"';
 end
-
-
-  # "SELECT color, COUNT (color) FROM bears GROUP BY color ORDER BY color DESC LIMIT 1";
