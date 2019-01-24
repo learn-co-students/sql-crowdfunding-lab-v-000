49a0d335a2c754e6c0f3095737977960b3aca94a
Sheena Bonilla <sheena.myb@gmail.com>
2019-01-24 02:47:37 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,70 @@
+/*insert 20 users*/
+INSERT INTO users VALUES (1, "Hermione", 13);
+INSERT INTO users VALUES (2, "Harry", 12);
+INSERT INTO users VALUES (3, "Ron", 10);
+INSERT INTO users VALUES (4, "Voldemort", 80);
+INSERT INTO users VALUES (5, "Severus", 40);
+INSERT INTO users VALUES (6, "Albus", 103);
+INSERT INTO users VALUES (7, "Hagrid", 38);
+INSERT INTO users VALUES (8, "Ginny", 8);
+INSERT INTO users VALUES (9, "Fred", 18);
+INSERT INTO users VALUES (10, "George", 17);
+INSERT INTO users VALUES (11, "Goyle", 14);
+INSERT INTO users VALUES (12, "Draco", 7);
+INSERT INTO users VALUES (13, "Luna", 11);
+INSERT INTO users VALUES (14, "Neville", 10);
+INSERT INTO users VALUES (15, "Newt", 26);
+INSERT INTO users VALUES (16, "James", 31);
+INSERT INTO users VALUES (17, "Lily", 30);
+INSERT INTO users VALUES (18, "Regulus", 22);
+INSERT INTO users VALUES (19, "Remus", 42);
+INSERT INTO users VALUES (20, "Hedwig", 4);
+
+  
+INSERT INTO projects VALUES (1, "Save the Hippos", "Animals", 100, 01012019, 06012019);
+INSERT INTO projects VALUES (2, "Help Me Read", "Education", 200, 01012019, 12312019);
+INSERT INTO projects VALUES (3, "Save the Rainforest", "Animals", 1000, 03012019, 06012019);
+INSERT INTO projects VALUES (4, "Teach America", "Education", 3500, 02012019, 02012020);
+INSERT INTO projects VALUES (5, "Harry Needs A Broom", "Transportation", 2000, 01012019, 03012019);
+INSERT INTO projects VALUES (6, "Find Dudley's Parents", "Children", 4000, 08012019, 12012019);
+INSERT INTO projects VALUES (7, "A New Wing for Hedwig", "Animals", 3300, 01012019, 10012019);
+INSERT INTO projects VALUES (8, "Fix the Weasley's Car", "Transportation", 10000, 02012019, 10012019);
+INSERT INTO projects VALUES (9, "A Hogwart's Christmas", "Education", 1000000, 01012019, 10012019);
+INSERT INTO projects VALUES (10, "Teach Guitar to Toddlers Overseas", "Education", 400000, 01012019, 06012019);
+
+
+/*insert 30 pledges*/
+INSERT INTO pledges VALUES (1)
+INSERT INTO pledges VALUES (2)
+INSERT INTO pledges VALUES (3)
+INSERT INTO pledges VALUES (4)
+INSERT INTO pledges VALUES (5)
+INSERT INTO pledges VALUES (6)
+INSERT INTO pledges VALUES (7)
+INSERT INTO pledges VALUES (8)
+INSERT INTO pledges VALUES (9)
+INSERT INTO pledges VALUES (10)
+INSERT INTO pledges VALUES (11)
+INSERT INTO pledges VALUES (12)
+INSERT INTO pledges VALUES (13)
+INSERT INTO pledges VALUES (14)
+INSERT INTO pledges VALUES (15)
+INSERT INTO pledges VALUES (16)
+INSERT INTO pledges VALUES (17)
+INSERT INTO pledges VALUES (18)
+INSERT INTO pledges VALUES (19)
+INSERT INTO pledges VALUES (20)
+INSERT INTO pledges VALUES (21)
+INSERT INTO pledges VALUES (22)
+INSERT INTO pledges VALUES (23)
+INSERT INTO pledges VALUES (24)
+INSERT INTO pledges VALUES (25)
+INSERT INTO pledges VALUES (26)
+INSERT INTO pledges VALUES (27)
+INSERT INTO pledges VALUES (28)
+INSERT INTO pledges VALUES (29)
+INSERT INTO pledges VALUES (30)
+  amount INTEGER,
+  user_id TEXT,
+  project_id INTEGER
+
