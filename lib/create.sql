26ef19284032acaf757eb7aff140295c191983ec
Karly Mareka <karly.mareka@gmail.com>
2019-03-26 23:10:28 +0000

    Done.

--- lib/create.sql
+++ lib/create.sql
@@ -13,4 +13,6 @@ age INTEGER);
 
 CREATE TABLE pledges (
 id INTEGER PRIMARY KEY,
-amount INTEGER); 
+amount INTEGER,
+user_id INTEGER,
+project_id INTEGER); 
---
+++ lib/insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
+(1, "Save the whales", "charity", 1000, 2019, 2020),
+(2, "Impeach the president", "politics", 20000, 2019, 2019),
+(3, "Help me go to Harvard", "charity", 50000, 2019, 2021),
+(4, "Graphing calculators for high schoolers", "charity", 500, 2019, 2019),
+(5, "Clean up garbage in the park", "community", 300, 2019, 2020),
+(6, "Common sense gun control", "politics", 900, 2019, 2021), 
+(7, "Reduce single use plastic waste", "community", 600, 2019, 2020),
+(8, "Baby clothes for refugees", "charity", 500, 2019, 2019),
+(9, "Pay off my cc debt", "charity", 1000000, 2019, 2021),
+(10, "Put speed bumps in school zone", "community", 500, 2019, 2019);
+
+INSERT INTO users(id, name, age) VALUES 
+(1, "Karly", 32), (2, "Matthew", 32), (3, "Ariel", 3), (4, "Adelia", 1), (5, "Scout", 8), (6, "Pumpkin", 6), (7, "Lisa", 57), (8, "Al", 62), (9, "Rosemary", 66), (10, "Joe", 68), (11, "Diane", 30), (12, "Pete", 31), (13, "Claire", 27), (14, "Jamie", 25), (15, "Frank", 30), (16, "Brianna", 0), (17, "John", 35), (18, "Abigail", 34), (19, "Faith", 25), (20, "Lara", 28); 
