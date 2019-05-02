687b3f0bc4fc305e0ba10ee2cba8b68e194ad8a6
jontenthoff <jontenthoff@hotmail.com>
2019-05-02 18:00:57 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO projects ("Blood", "Small", 120000, 4122019, 8122019);
+INSERT INTO projects ("Radio", "Medium", 250000, 3292019, 7122019);
+INSERT INTO projects ("Auction", "Large", 500000, 112019, 12312019);
