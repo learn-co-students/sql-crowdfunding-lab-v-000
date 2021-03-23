1f055437222e94adb7159c7755959ccddf541ec2
Daniel Suarez <suarez.daniel101@gmail.com>
2021-03-23 12:23:07 -0400

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,10 +21,11 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT title, pledges.amount FROM projects
+  "SELECT title, (pledges.amount - funding_goal) FROM projects
     JOIN pledges ON projects.id = pledges.project_id
+    WHERE (pledges.amount - funding_goal) = funding_goal
     GROUP BY title
-    HAVING SUM(pledges.amount) >= funding_goal"
+    ORDER BY title"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
