7226316a56db6baa34fed26bcca56840ece3e70f
Daniel Suarez <suarez.daniel101@gmail.com>
2021-03-23 00:26:40 -0400

    Done.

--- lib/create.sql
+++ lib/create.sql
@@ -7,3 +7,16 @@ CREATE TABLE projects (
     end_date INTEGER
 );
 
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER
+);
+
+CREATE TABLE pledges (
+    id INTEGER PRIMARY KEY,
+    amount INTEGER,
+    user_id INTEGER,
+    project_id INTEGER
+);
+
